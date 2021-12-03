02f9ea164ff96f9798168906face2b3300df9417
Stephan Aßmus <stephan.assmus@sap.com>
2021-12-03 14:37:26 +0100

    solution part 1

--- 2021/day-03/main.go
+++ 2021/day-03/main.go
@@ -1,12 +1,103 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"io/ioutil"
+	"math"
 	"strings"
 )
 
+type BitCounts struct {
+	ones  int
+	zeros int
+}
+
+var indexOutOfRangeErr = errors.New("index out of range")
+
+func getBitCountsAtColumn(binaryNumbers []string, column int) (BitCounts, error) {
+	result := BitCounts{}
+	if len(binaryNumbers) == 0 {
+		return result, fmt.Errorf("no input binary numbers")
+	}
+	if len(binaryNumbers[0]) <= column {
+		return result, indexOutOfRangeErr
+	}
+	for _, binaryNumber := range binaryNumbers {
+		if binaryNumber == "" {
+			continue
+		}
+
+		switch binaryNumber[column] {
+		case '0':
+			result.zeros++
+		case '1':
+			result.ones++
+		}
+	}
+	return result, nil
+}
+
+func getUsedBits(bitCounts []BitCounts, most bool) string {
+	result := ""
+	for _, digit := range bitCounts {
+		if digit.ones > digit.zeros {
+			if most {
+				result += "1"
+			} else {
+				result += "0"
+			}
+		} else {
+			if most {
+				result += "0"
+			} else {
+				result += "1"
+			}
+		}
+	}
+	return result
+}
+
+func getBitCounts(binaryNumbers []string, columnCount int) []BitCounts {
+	var bitCountsByColumns []BitCounts
+	for column := 0; column < columnCount; column++ {
+		bitCounts, err := getBitCountsAtColumn(binaryNumbers, column)
+		if err != nil {
+			if err == indexOutOfRangeErr {
+				break
+			}
+			exitIfError(err)
+		}
+		bitCountsByColumns = append(bitCountsByColumns, bitCounts)
+	}
+	return bitCountsByColumns
+}
+
+func getGammaAndEpsilon(binaryNumbers []string, columnCount int) (gamma, epsilon string) {
+	bitCounts := getBitCounts(binaryNumbers, columnCount)
+	gamma = getUsedBits(bitCounts, true)
+	epsilon = getUsedBits(bitCounts, false)
+	return
+}
+
+func toDecimal(binary string) int {
+	result := 0
+	for index := 1; index <= len(binary); index++ {
+		bit := binary[len(binary)-index]
+		if bit == '1' {
+			result += int(math.Pow(2, float64(index-1)))
+		}
+	}
+	return result
+}
 
 func main() {
+	binaryNumbers := loadInput("binary-input.txt")
+	gamma, epsilon := getGammaAndEpsilon(binaryNumbers, 12)
+	gammaDecimal := toDecimal(gamma)
+	epsilonDecimal := toDecimal(epsilon)
+	fmt.Printf("gamma: %s (%v), epsilon: %s (%v)\n", gamma, gammaDecimal, epsilon, epsilonDecimal)
+	fmt.Printf("power: %v\n", gammaDecimal*epsilonDecimal)
 }
 
 func loadInput(filename string) []string {
--- 2021/day-03/main_test.go
+++ 2021/day-03/main_test.go
@@ -5,6 +5,26 @@ import (
 	"testing"
 )
 
-func Test_XX(t *testing.T) {
-	assert.True(t, true)
+func Test_getGammaAndEpsilon(t *testing.T) {
+	exampleInput := []string{
+		"00100",
+		"11110",
+		"10110",
+		"10111",
+		"10101",
+		"01111",
+		"00111",
+		"11100",
+		"10000",
+		"11001",
+		"00010",
+		"01010"}
+	gamma, epsilon := getGammaAndEpsilon(exampleInput, 5)
+	assert.Equal(t, "10110", gamma)
+	assert.Equal(t, "01001", epsilon)
+}
+
+func Test_toDecimal(t *testing.T) {
+	assert.Equal(t, 22, toDecimal("10110"))
+	assert.Equal(t, 9, toDecimal("01001"))
 }
