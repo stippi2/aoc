9a7318d97a403ba827345a2950485f61dcbe2c86
Stephan Aßmus <stephan.assmus@sap.com>
2021-12-04 12:45:49 +0100

    Parsing bingo-input and util methods

---
+++ 2021/day-04/main.go
@@ -0,0 +1,140 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+type Board struct {
+	size    int
+	numbers []int
+	hits    []bool
+}
+
+func (b *Board) init(size int) {
+	b.size = size
+	b.numbers = make([]int, size*size)
+	b.hits = make([]bool, size*size)
+}
+
+func (b *Board) offset(x, y int) int {
+	return y*b.size + x
+}
+
+func (b *Board) setNumber(x, y, number int) {
+	b.numbers[b.offset(x, y)] = number
+}
+
+func (b *Board) numberAt(x, y int) int {
+	return b.numbers[b.offset(x, y)]
+}
+
+func (b *Board) markFieldsWithNumber(number int) bool {
+	changed := false
+	for y := 0; y < b.size; y++ {
+		for x := 0; x < b.size; x++ {
+			if b.numberAt(x, y) == number {
+				changed = changed || !b.hits[b.offset(x, y)]
+				b.hits[b.offset(x, y)] = true
+			}
+		}
+	}
+	return changed
+}
+
+func (b *Board) testCompletedRows() bool {
+	for y := 0; y < b.size; y++ {
+		hitsPerRow := 0
+		for x := 0; x < b.size; x++ {
+			if b.hits[b.offset(x, y)] {
+				hitsPerRow++
+			}
+		}
+		if hitsPerRow == 5 {
+			return true
+		}
+	}
+	for x := 0; x < b.size; x++ {
+		hitsPerColumn := 0
+		for y := 0; y < b.size; y++ {
+			if b.hits[b.offset(x, y)] {
+				hitsPerColumn++
+			}
+		}
+		if hitsPerColumn == 5 {
+			return true
+		}
+	}
+	return false
+}
+
+func (b *Board) sumOfUnmarkedFields() int {
+	sum := 0
+	for offset := 0; offset < b.size*b.size; offset++ {
+		if !b.hits[offset] {
+			sum += b.numbers[offset]
+		}
+	}
+	return sum
+}
+
+func (b *Board) setRow(y int, numberRow string) {
+	numberRow = strings.TrimSpace(numberRow)
+	numbers := strings.Split(numberRow, " ")
+	x := 0
+	for _, number := range numbers {
+		if number == "" {
+			// there are double spaces in the rows leading to empty "numbers"
+			continue
+		}
+		v, err := strconv.Atoi(number)
+		if err != nil {
+			panic(fmt.Sprintf("failed to parse number at column %v ('%s'): %v", x, number, err))
+		}
+		b.setNumber(x, y, v)
+		x++
+	}
+}
+
+func main() {
+	//bingoInput := loadInput("bingo-input.txt")
+}
+
+func parseBingoInput(input string) (numberSequence []int, boards []Board) {
+	parts := strings.Split(input, "\n\n")
+	numbers := strings.Split(strings.TrimSpace(parts[0]), ",")
+	numberSequence = make([]int, len(numbers))
+	for i, number := range numbers {
+		var err error
+		numberSequence[i], err = strconv.Atoi(number)
+		if err != nil {
+			panic(fmt.Sprintf("failed to parse number at index %v ('%s'): %v", i, number, err))
+		}
+	}
+
+	parts = parts[1:]
+	boards = make([]Board, len(parts))
+	for i, part := range parts {
+		rows := strings.Split(strings.TrimSpace(part), "\n")
+		boards[i].init(len(rows))
+		for y, row := range rows {
+			boards[i].setRow(y, row)
+		}
+	}
+	return
+}
+
+func loadInput(filename string) string {
+	fileContents, err := ioutil.ReadFile(filename)
+	exitIfError(err)
+
+	return strings.Trim(string(fileContents), "\n")
+}
+
+func exitIfError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
