444e8454d25e6c1e50e8f00f470bd0097eb93d59
Stephan Aßmus <stephan.assmus@sap.com>
2021-12-06 09:01:27 +0100

    Solution day 6 part 1

--- 2021/day-06/main.go
+++ 2021/day-06/main.go
@@ -1,15 +1,60 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
+	"strconv"
 	"strings"
 )
 
-func main() {
+func countLanternFish(countsPerAge []int) int {
+	count := 0
+	for i := 0; i < 9; i++ {
+		count += countsPerAge[i]
+	}
+	return count
 }
 
-func parseLanternFishAges(input string) (numberSequence []int) {
-	return nil
+func simulateAgingAndReproduction(countsPerAge []int) (newCountsPerAge []int) {
+	newCountsPerAge = make([]int, 9)
+	// simulate aging by shifting the array
+	// (number of fish at index 1 (age = 1) are placed at index 0 (age = 0)
+	// ... and so forth
+	for i := 1; i < 9; i++ {
+		newCountsPerAge[i-1] = countsPerAge[i]
+	}
+	// simulate the reproduction by adding the number of fish with (age == 0) to age == 8
+	newCountsPerAge[8] = countsPerAge[0]
+	// rotate the fish with day == 0 back to day == 6
+	newCountsPerAge[6] += countsPerAge[0]
+	return
+}
+
+func initAgeCounts(individualAges []int) (countsPerAge []int) {
+	countsPerAge = make([]int, 9)
+	for _, individualAge := range individualAges {
+		if individualAge >= 0 && individualAge < len(countsPerAge) {
+			countsPerAge[individualAge]++
+		}
+	}
+	return
+}
+
+func main() {
+	countsPerAge := initAgeCounts(parseLanternFishAges(loadInput("lanternfish-ages.txt")))
+	for i := 0; i < 80; i++ {
+		countsPerAge = simulateAgingAndReproduction(countsPerAge)
+	}
+	fmt.Printf("Number of lantern fish after 80 days: %v\n", countLanternFish(countsPerAge))
+}
+
+func parseLanternFishAges(input string) (individualAges []int) {
+	numbers := strings.Split(input, ",")
+	individualAges = make([]int, len(numbers))
+	for i, numberString := range numbers {
+		individualAges[i], _ = strconv.Atoi(numberString)
+	}
+	return
 }
 
 func loadInput(filename string) string {
--- 2021/day-06/main_test.go
+++ 2021/day-06/main_test.go
@@ -6,7 +6,31 @@ import (
 )
 
 var exampleInput = `3,4,3,1,2`
+var agesAfter18Days = `6,0,6,4,5,6,0,1,1,2,6,0,1,1,1,2,2,3,3,4,6,7,8,8,8,8`
+
+// 0 = 3
+// 1 = 5
+// 2 = 3
+// 3 = 2
+// 4 = 2
+// 5 = 1
+// 6 = 5
+// 7 = 1
+// 8 = 4
 
 func Test_parseLanternFishAges(t *testing.T) {
-	assert.Len(t, exampleInput, 5)
+	assert.Equal(t, []int{3, 4, 3, 1, 2}, parseLanternFishAges(exampleInput))
+}
+
+func Test_initAgeCounts(t *testing.T) {
+	assert.Equal(t, []int{0, 1, 1, 2, 1, 0, 0, 0, 0}, initAgeCounts(parseLanternFishAges(exampleInput)))
+}
+
+func Test_simulateAgingAndReproduction(t *testing.T) {
+	countsPerAge := initAgeCounts(parseLanternFishAges(exampleInput))
+	for i := 0; i < 18; i++ {
+		countsPerAge = simulateAgingAndReproduction(countsPerAge)
+	}
+	assert.Equal(t, initAgeCounts(parseLanternFishAges(agesAfter18Days)), countsPerAge)
+	assert.Equal(t, 26, countLanternFish(countsPerAge))
 }
