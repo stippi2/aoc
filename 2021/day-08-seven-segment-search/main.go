dd357f6ba333302a58f5ec1f5ad04eb50bfbf0ec
Stephan Aßmus <stephan.assmus@sap.com>
2021-12-09 08:25:59 +0100

    parse input

--- 2021/day-08-seven-segment-search/main.go
+++ 2021/day-08-seven-segment-search/main.go
@@ -2,14 +2,50 @@ package main
 
 import (
 	"io/ioutil"
+	"strconv"
 	"strings"
 )
+
+type Map struct {
+	width  int
+	height int
+	data   []int
+}
+
+func (m *Map) init(width, height int) {
+	m.width = width
+	m.height = height
+	m.data = make([]int, width*height)
+}
+
+func (m *Map) offset(x, y int) int {
+	return m.height*y + x
+}
+
+func (m *Map) set(x, y, value int) {
+	m.data[m.offset(x, y)] = value
+}
+
+func (m *Map) get(x, y int) int {
+	return m.data[m.offset(x, y)]
+}
 
 func main() {
 }
 
-func parseInput(input string) []string {
-	return strings.Split(input, "\n")
+func parseInput(input string) Map {
+	lines := strings.Split(input, "\n")
+	var m Map
+	m.init(len(lines[0]), len(lines))
+	for y, line := range lines {
+		for x, c := range strings.Split(line, "") {
+			value, err := strconv.Atoi(c)
+			if err == nil {
+				m.set(x, y, value)
+			}
+		}
+	}
+	return m
 }
 
 func loadInput(filename string) string {
--- 2021/day-08-seven-segment-search/main_test.go
+++ 2021/day-08-seven-segment-search/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
@@ -11,4 +12,10 @@ var exampleInput = `2199943210
 9899965678`
 
 func Test_parseInput(t *testing.T) {
+	m := parseInput(exampleInput)
+	assert.Equal(t, 10, m.width)
+	assert.Equal(t, 5, m.height)
+	assert.Equal(t, 9, m.get(1, 1))
+	assert.Equal(t, 9, m.get(1, 1))
+	assert.Equal(t, 8, m.get(9, 4))
 }
