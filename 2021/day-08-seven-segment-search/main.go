ad7fbccd2815a672d34703ff268cb7e56ac35923
Stephan Aßmus <stephan.assmus@sap.com>
2021-12-09 09:25:36 +0100

    Fix editing wrong day

--- 2021/day-08-seven-segment-search/main.go
+++ 2021/day-08-seven-segment-search/main.go
@@ -3,77 +3,202 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"math"
-	"strconv"
 	"strings"
 )
 
-type Map struct {
-	width  int
-	height int
-	data   []int
+type Display struct {
+	// readings contains strings with varying number of elements for all ten digits
+	readings []string
+	// digits contains 4 digits (out of the set of readings) representing the current 4-digit value of the display
+	digits []string
+	// mapping is the deduced mapping of signals to digit components
+	//  aaaa
+	// b    c
+	// b    c
+	//  dddd
+	// e    f
+	// e    f
+	//  gggg
+	// If it can be deduced that the signal "e" should be mapped to the digit component "a",
+	// then this key-value pair would be contained in mapping.
+	mapping map[string]string
 }
 
-func (m *Map) init(width, height int) {
-	m.width = width
-	m.height = height
-	m.data = make([]int, width*height)
-}
-
-func (m *Map) offset(x, y int) int {
-	return m.width*y + x
-}
-
-func (m *Map) set(x, y, value int) {
-	m.data[m.offset(x, y)] = value
-}
-
-func (m *Map) get(x, y int) int {
-	if x < 0 || x >= m.width || y < 0 || y >= m.height {
-		return math.MaxInt32
+func maskDigit(digit, mask string) string {
+	for _, char := range strings.Split(mask, "") {
+		digit = strings.ReplaceAll(digit, char, "")
 	}
-	return m.data[m.offset(x, y)]
+	return digit
 }
 
-func (m *Map) isLowPoint(x, y int) bool {
-	value := m.get(x, y)
-	return value < m.get(x-1, y) &&
-		value < m.get(x+1, y) &&
-		value < m.get(x, y-1) &&
-		value < m.get(x, y+1)
+func (d *Display) deduceMapping() {
+	// Deduce the mapping for "a"
+	one := d.findFirstDigit(2)
+	seven := d.findFirstDigit(3)
+	a := maskDigit(seven, one)
+	d.mapping["a"] = a
+
+	// Removing the elements of 7 from those of 4 leaves "bd"
+	four := d.findFirstDigit(4)
+	bd := maskDigit(four, seven)
+
+	// countPerElement tracks how often a given element occurs across all readings
+	// In an unscrambled display, the distribution is like this:
+	// a = 8
+	// b = 6
+	// c = 8
+	// d = 7
+	// e = 4
+	// f = 9
+	// g = 7
+	countPerElement := make(map[string]int)
+	for _, reading := range d.readings {
+		for _, element := range strings.Split(reading, "") {
+			countPerElement[element]++
+		}
+	}
+
+	// Deduce the elements by occurrences
+	for element, count := range countPerElement {
+		switch count {
+		case 4:
+			d.mapping["e"] = element
+		case 6:
+			d.mapping["b"] = element
+		case 7:
+			if strings.Contains(bd, element) {
+				d.mapping["d"] = element
+			} else {
+				d.mapping["g"] = element
+			}
+		case 8:
+			if element != d.mapping["a"] {
+				d.mapping["c"] = element
+			}
+		case 9:
+			d.mapping["f"] = element
+		}
+	}
 }
 
-func (m *Map) sumRiskLevels() int {
+func (d *Display) findFirstDigit(length int) string {
+	for _, reading := range d.readings {
+		if len(reading) == length {
+			return reading
+		}
+	}
+	return ""
+}
+
+func countDigits(displays []Display, condition func(digit string) bool) int {
 	sum := 0
-	for y := 0; y < m.height; y++ {
-		for x := 0; x < m.width; x++ {
-			if m.isLowPoint(x, y) {
-				sum += 1 + m.get(x, y)
+	for _, display := range displays {
+		for _, digit := range display.digits {
+			if condition(digit) {
+				sum++
 			}
 		}
 	}
 	return sum
 }
 
-func main() {
-	m := parseInput(loadInput("puzzle-input.txt"))
-	riskLevelSum := m.sumRiskLevels()
-	fmt.Printf("sum of risk levels: %v\n", riskLevelSum)
+func conditionOnesFoursSevensAndEights(digit string) bool {
+	switch len(digit) {
+	case 2, 4, 3, 7:
+		return true
+	}
+	return false
 }
 
-func parseInput(input string) Map {
-	lines := strings.Split(input, "\n")
-	var m Map
-	m.init(len(lines[0]), len(lines))
-	for y, line := range lines {
-		for x, c := range strings.Split(line, "") {
-			value, err := strconv.Atoi(c)
-			if err == nil {
-				m.set(x, y, value)
-			}
+func (d *Display) scramble(unscrambled string) string {
+	scrambled := ""
+	for _, element := range strings.Split(unscrambled, "") {
+		scrambled += d.mapping[element]
+	}
+	return scrambled
+}
+
+func (d *Display) digitToInt() map[string]int {
+	digitToInt := make(map[string]int)
+	// 0: abc.efg  6
+	// 1: ..c..f.  2 *
+	// 2: a.cde.g  5
+	// 3: a.cd.fg  5
+	// 4: .bcd.f.  4 *
+	// 5: ab.d.fg  5
+	// 6: ab.defg  6
+	// 7: a.c..f.  3 *
+	// 8: abcdefg  7 *
+	// 9: abcd.fg  6
+	digitToInt[d.scramble("abcefg")] = 0
+	digitToInt[d.scramble("cf")] = 1
+	digitToInt[d.scramble("acdeg")] = 2
+	digitToInt[d.scramble("acdfg")] = 3
+	digitToInt[d.scramble("bcdf")] = 4
+	digitToInt[d.scramble("abdfg")] = 5
+	digitToInt[d.scramble("abdefg")] = 6
+	digitToInt[d.scramble("acf")] = 7
+	digitToInt[d.scramble("abcdefg")] = 8
+	digitToInt[d.scramble("abcdfg")] = 9
+	return digitToInt
+}
+
+func hasSameChars(a, b string) bool {
+	// Assumes each char is only contained once in each string!
+	if len(a) != len(b) {
+		return false
+	}
+	found := 0
+	for _, char := range strings.Split(a, "") {
+		if strings.Contains(b, char) {
+			found++
 		}
 	}
-	return m
+	return found == len(a)
+}
+
+func findValue(scrambledDigit string, mapping map[string]int) int {
+	for key, value := range mapping {
+		if hasSameChars(key, scrambledDigit) {
+			return value
+		}
+	}
+	panic("did not find value in mapping")
+}
+
+func (d *Display) descramble() int {
+	mapping := d.digitToInt()
+	result := 0
+	result += findValue(d.digits[0], mapping) * 1000
+	result += findValue(d.digits[1], mapping) * 100
+	result += findValue(d.digits[2], mapping) * 10
+	result += findValue(d.digits[3], mapping) * 1
+	return result
+}
+
+func main() {
+	displays := parseInput(loadInput("puzzle-input.txt"))
+	count := countDigits(displays, conditionOnesFoursSevensAndEights)
+	fmt.Printf("number of 1, 4, 7, and 8: %v\n", count)
+
+	sum := 0
+	for _, d := range displays {
+		d.deduceMapping()
+		sum += d.descramble()
+	}
+	fmt.Printf("sum of all output values: %v\n", sum)
+}
+
+func parseInput(input string) (displays []Display) {
+	lines := strings.Split(input, "\n")
+	displays = make([]Display, len(lines))
+	for i, line := range lines {
+		signalsDigits := strings.Split(line, " | ")
+		displays[i].readings = strings.Split(signalsDigits[0], " ")
+		displays[i].digits = strings.Split(signalsDigits[1], " ")
+		displays[i].mapping = make(map[string]string)
+	}
+	return
 }
 
 func loadInput(filename string) string {
--- 2021/day-09-smoke-basin/main.go
+++ 2021/day-09-smoke-basin/main.go
@@ -3,202 +3,111 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math"
+	"strconv"
 	"strings"
 )
 
-type Display struct {
-	// readings contains strings with varying number of elements for all ten digits
-	readings []string
-	// digits contains 4 digits (out of the set of readings) representing the current 4-digit value of the display
-	digits []string
-	// mapping is the deduced mapping of signals to digit components
-	//  aaaa
-	// b    c
-	// b    c
-	//  dddd
-	// e    f
-	// e    f
-	//  gggg
-	// If it can be deduced that the signal "e" should be mapped to the digit component "a",
-	// then this key-value pair would be contained in mapping.
-	mapping map[string]string
+type Point struct {
+	x, y int
 }
 
-func maskDigit(digit, mask string) string {
-	for _, char := range strings.Split(mask, "") {
-		digit = strings.ReplaceAll(digit, char, "")
+type Map struct {
+	width   int
+	height  int
+	data    []int
+	visited map[Point]bool
+}
+
+func (m *Map) init(width, height int) {
+	m.width = width
+	m.height = height
+	m.data = make([]int, width*height)
+}
+
+func (m *Map) offset(x, y int) int {
+	return m.width*y + x
+}
+
+func (m *Map) set(x, y, value int) {
+	m.data[m.offset(x, y)] = value
+}
+
+func (m *Map) get(x, y int) int {
+	if x < 0 || x >= m.width || y < 0 || y >= m.height {
+		return math.MaxInt32
 	}
-	return digit
+	return m.data[m.offset(x, y)]
 }
 
-func (d *Display) deduceMapping() {
-	// Deduce the mapping for "a"
-	one := d.findFirstDigit(2)
-	seven := d.findFirstDigit(3)
-	a := maskDigit(seven, one)
-	d.mapping["a"] = a
+func (m *Map) isLowPoint(x, y int) bool {
+	value := m.get(x, y)
+	return value < m.get(x-1, y) &&
+		value < m.get(x+1, y) &&
+		value < m.get(x, y-1) &&
+		value < m.get(x, y+1)
+}
 
-	// Removing the elements of 7 from those of 4 leaves "bd"
-	four := d.findFirstDigit(4)
-	bd := maskDigit(four, seven)
+func (m *Map) floodBasin(x, y int) (size int) {
+	visited := make(map[Point]bool)
+	var queue []Point
+	queue = append(queue, Point{x, y})
 
-	// countPerElement tracks how often a given element occurs across all readings
-	// In an unscrambled display, the distribution is like this:
-	// a = 8
-	// b = 6
-	// c = 8
-	// d = 7
-	// e = 4
-	// f = 9
-	// g = 7
-	countPerElement := make(map[string]int)
-	for _, reading := range d.readings {
-		for _, element := range strings.Split(reading, "") {
-			countPerElement[element]++
+	for len(queue) > 0 {
+		p := queue[0]
+		size += 1
+		visited[p] = true
+		fmt.Printf("visiting %v, queue: %v\n", p, queue)
+
+		neighbors := []Point{
+			{p.x - 1, p.y},
+			{p.x + 1, p.y},
+			{p.x, p.y - 1},
+			{p.x, p.y + 1},
 		}
-	}
 
-	// Deduce the elements by occurrences
-	for element, count := range countPerElement {
-		switch count {
-		case 4:
-			d.mapping["e"] = element
-		case 6:
-			d.mapping["b"] = element
-		case 7:
-			if strings.Contains(bd, element) {
-				d.mapping["d"] = element
-			} else {
-				d.mapping["g"] = element
+		for _, neighbor := range neighbors {
+			if !visited[neighbor] && m.get(neighbor.x, neighbor.y) < 9 {
+				queue = append(queue, neighbor)
 			}
-		case 8:
-			if element != d.mapping["a"] {
-				d.mapping["c"] = element
-			}
-		case 9:
-			d.mapping["f"] = element
 		}
+
+		queue = queue[1:]
 	}
+	return
 }
 
-func (d *Display) findFirstDigit(length int) string {
-	for _, reading := range d.readings {
-		if len(reading) == length {
-			return reading
-		}
-	}
-	return ""
-}
-
-func countDigits(displays []Display, condition func(digit string) bool) int {
+func (m *Map) sumRiskLevels() int {
 	sum := 0
-	for _, display := range displays {
-		for _, digit := range display.digits {
-			if condition(digit) {
-				sum++
+	for y := 0; y < m.height; y++ {
+		for x := 0; x < m.width; x++ {
+			if m.isLowPoint(x, y) {
+				sum += 1 + m.get(x, y)
 			}
 		}
 	}
 	return sum
 }
 
-func conditionOnesFoursSevensAndEights(digit string) bool {
-	switch len(digit) {
-	case 2, 4, 3, 7:
-		return true
-	}
-	return false
-}
-
-func (d *Display) scramble(unscrambled string) string {
-	scrambled := ""
-	for _, element := range strings.Split(unscrambled, "") {
-		scrambled += d.mapping[element]
-	}
-	return scrambled
-}
-
-func (d *Display) digitToInt() map[string]int {
-	digitToInt := make(map[string]int)
-	// 0: abc.efg  6
-	// 1: ..c..f.  2 *
-	// 2: a.cde.g  5
-	// 3: a.cd.fg  5
-	// 4: .bcd.f.  4 *
-	// 5: ab.d.fg  5
-	// 6: ab.defg  6
-	// 7: a.c..f.  3 *
-	// 8: abcdefg  7 *
-	// 9: abcd.fg  6
-	digitToInt[d.scramble("abcefg")] = 0
-	digitToInt[d.scramble("cf")] = 1
-	digitToInt[d.scramble("acdeg")] = 2
-	digitToInt[d.scramble("acdfg")] = 3
-	digitToInt[d.scramble("bcdf")] = 4
-	digitToInt[d.scramble("abdfg")] = 5
-	digitToInt[d.scramble("abdefg")] = 6
-	digitToInt[d.scramble("acf")] = 7
-	digitToInt[d.scramble("abcdefg")] = 8
-	digitToInt[d.scramble("abcdfg")] = 9
-	return digitToInt
-}
-
-func hasSameChars(a, b string) bool {
-	// Assumes each char is only contained once in each string!
-	if len(a) != len(b) {
-		return false
-	}
-	found := 0
-	for _, char := range strings.Split(a, "") {
-		if strings.Contains(b, char) {
-			found++
-		}
-	}
-	return found == len(a)
-}
-
-func findValue(scrambledDigit string, mapping map[string]int) int {
-	for key, value := range mapping {
-		if hasSameChars(key, scrambledDigit) {
-			return value
-		}
-	}
-	panic("did not find value in mapping")
-}
-
-func (d *Display) descramble() int {
-	mapping := d.digitToInt()
-	result := 0
-	result += findValue(d.digits[0], mapping) * 1000
-	result += findValue(d.digits[1], mapping) * 100
-	result += findValue(d.digits[2], mapping) * 10
-	result += findValue(d.digits[3], mapping) * 1
-	return result
-}
-
 func main() {
-	displays := parseInput(loadInput("puzzle-input.txt"))
-	count := countDigits(displays, conditionOnesFoursSevensAndEights)
-	fmt.Printf("number of 1, 4, 7, and 8: %v\n", count)
-
-	sum := 0
-	for _, d := range displays {
-		d.deduceMapping()
-		sum += d.descramble()
-	}
-	fmt.Printf("sum of all output values: %v\n", sum)
+	m := parseInput(loadInput("puzzle-input.txt"))
+	riskLevelSum := m.sumRiskLevels()
+	fmt.Printf("sum of risk levels: %v\n", riskLevelSum)
 }
 
-func parseInput(input string) (displays []Display) {
+func parseInput(input string) Map {
 	lines := strings.Split(input, "\n")
-	displays = make([]Display, len(lines))
-	for i, line := range lines {
-		signalsDigits := strings.Split(line, " | ")
-		displays[i].readings = strings.Split(signalsDigits[0], " ")
-		displays[i].digits = strings.Split(signalsDigits[1], " ")
-		displays[i].mapping = make(map[string]string)
+	var m Map
+	m.init(len(lines[0]), len(lines))
+	for y, line := range lines {
+		for x, c := range strings.Split(line, "") {
+			value, err := strconv.Atoi(c)
+			if err == nil {
+				m.set(x, y, value)
+			}
+		}
 	}
-	return
+	return m
 }
 
 func loadInput(filename string) string {
