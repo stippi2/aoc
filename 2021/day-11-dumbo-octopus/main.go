e74353378807befa624fcee29a8ef76468b61c12
Stephan Aßmus <stephan.assmus@sap.com>
2021-12-11 14:37:38 +0100

    Map type and parsing

--- 2021/day-11-dumbo-octopus/main.go
+++ 2021/day-11-dumbo-octopus/main.go
@@ -2,14 +2,59 @@ package main
 
 import (
 	"io/ioutil"
+	"strconv"
 	"strings"
 )
+
+type Map struct {
+	width  int
+	height int
+	data   []int
+}
+
+func (m *Map) init(width, height int) {
+	m.width = width
+	m.height = height
+	m.data = make([]int, width*height)
+}
+
+func (m *Map) offset(x, y int) int {
+	return m.width*y + x
+}
+
+func (m *Map) set(x, y, value int) {
+	m.data[m.offset(x, y)] = value
+}
+
+func (m *Map) get(x, y int) int {
+	return m.data[m.offset(x, y)]
+}
 
 func main() {
 }
 
-func parseInput(input string) (lines []string) {
-	return strings.Split(input, "\n")
+func parseInput(input string) *Map {
+	lines := strings.Split(input, "\n")
+	height := len(lines)
+	if height == 0 {
+		return nil
+	}
+	width := len(lines[0])
+	if width == 0 {
+		return nil
+	}
+	m := Map{}
+	m.init(width, height)
+	for y := 0; y < height; y++ {
+		for x, char := range strings.Split(lines[y], "") {
+			if x >= width {
+				break
+			}
+			v, _ := strconv.Atoi(char)
+			m.set(x, y, v)
+		}
+	}
+	return &m
 }
 
 func loadInput(filename string) string {
--- 2021/day-11-dumbo-octopus/main_test.go
+++ 2021/day-11-dumbo-octopus/main_test.go
@@ -16,6 +16,22 @@ var exampleInput = `5483143223
 4846848554
 5283751526`
 
-func Test(t *testing.T) {
-	assert.True(t, true)
+var smallInput = `11111
+19991
+19191
+19991
+11111`
+
+var expectedAfterOneStep = `34543
+40004
+50005
+40004
+34543`
+
+func Test_parseInput(t *testing.T) {
+	m := parseInput(exampleInput)
+	assert.Equal(t, 10, m.width)
+	assert.Equal(t, 10, m.height)
+	assert.Equal(t, 5, m.get(0, 0))
+	assert.Equal(t, 6, m.get(9, 9))
 }
