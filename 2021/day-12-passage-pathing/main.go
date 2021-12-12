3a0cd62ba46e99618e20ccc04029ab4cd5ae3d0e
Stephan Aßmus <stephan.assmus@sap.com>
2021-12-12 08:40:02 +0100

    parsing works

--- 2021/day-12-passage-pathing/main.go
+++ 2021/day-12-passage-pathing/main.go
@@ -1,23 +1,37 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
-	"strconv"
 	"strings"
 )
 
 type Node struct {
 	name string
-	next []Node
+	next []*Node
 }
 
 func main() {
 }
 
+func getNode(nodeMap map[string]*Node, name string) *Node {
+	node := nodeMap[name]
+	if node == nil {
+		node = &Node{name: name}
+		nodeMap[name] = node
+	}
+	return node
+}
+
 func parseInput(input string) *Node {
+	allNodes := make(map[string]*Node)
 	lines := strings.Split(input, "\n")
-	return nil
+	for _, line := range lines {
+		nodeNames := strings.Split(line, "-")
+		a := getNode(allNodes, nodeNames[0])
+		b := getNode(allNodes, nodeNames[1])
+		a.next = append(a.next, b)
+	}
+	return allNodes["start"]
 }
 
 func loadInput(filename string) string {
--- 2021/day-12-passage-pathing/main_test.go
+++ 2021/day-12-passage-pathing/main_test.go
@@ -8,6 +8,10 @@ import (
 func Test_parseInput(t *testing.T) {
 	n := parseInput(exampleInput)
 	assert.Equal(t, "start", n.name)
+	if assert.Equal(t, 2, len(n.next)) {
+		assert.Equal(t, "A", n.next[0].name)
+		assert.Equal(t, "b", n.next[1].name)
+	}
 }
 
 var exampleInput = `start-A
