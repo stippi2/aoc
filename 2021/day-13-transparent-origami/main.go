dd9e10f19ac2965a5e21b781fe1e1be66302153e
Stephan Aßmus <stephan.assmus@sap.com>
2021-12-13 18:58:55 +0100

    Parsing works

--- 2021/day-13-transparent-origami/main.go
+++ 2021/day-13-transparent-origami/main.go
@@ -2,18 +2,51 @@ package main
 
 import (
 	"io/ioutil"
+	"strconv"
 	"strings"
 )
 
-type Origami struct {
+type Point struct {
+	x, y int
+}
 
+type Origami struct {
+	dots   []Point
+	foldsY []int
+	foldsX []int
 }
 
 func main() {
 }
 
-func parseInput(input string) *Origami {
-	return nil
+func parseInput(input string) Origami {
+	inputSections := strings.Split(input, "\n\n")
+	if len(inputSections) != 2 {
+		panic("expected sections in the input separated by a double line-break")
+	}
+
+	origami := Origami{}
+
+	dots := strings.Split(inputSections[0], "\n")
+	for _, dot := range dots {
+		coords := strings.Split(dot, ",")
+		x, _ := strconv.Atoi(coords[0])
+		y, _ := strconv.Atoi(coords[1])
+		origami.dots = append(origami.dots, Point{x, y})
+	}
+
+	folds := strings.Split(inputSections[1], "\n")
+	for _, fold := range folds {
+		parts := strings.Split(fold, "=")
+		v, _ := strconv.Atoi(parts[1])
+		switch parts[0] {
+		case "fold along y":
+			origami.foldsY = append(origami.foldsY, v)
+		case "fold along x":
+			origami.foldsX = append(origami.foldsX, v)
+		}
+	}
+	return origami
 }
 
 func loadInput(filename string) string {
--- 2021/day-13-transparent-origami/main_test.go
+++ 2021/day-13-transparent-origami/main_test.go
@@ -5,6 +5,38 @@ import (
 	"testing"
 )
 
+var exampleInput = `6,10
+0,14
+9,10
+0,3
+10,4
+4,11
+6,0
+6,12
+4,1
+0,13
+10,12
+3,4
+3,0
+8,4
+1,10
+2,14
+8,10
+9,0
+
+fold along y=7
+fold along x=5`
+
 func Test_parseInput(t *testing.T) {
-	assert.True(t, true)
+	o := parseInput(exampleInput)
+	if assert.Len(t, o.dots, 18) {
+		assert.Equal(t, o.dots[0], Point{6, 10})
+		assert.Equal(t, o.dots[17], Point{9, 0})
+	}
+	if assert.Len(t, o.foldsY, 1) {
+		assert.Equal(t, o.foldsY[0], 7)
+	}
+	if assert.Len(t, o.foldsX, 1) {
+		assert.Equal(t, o.foldsX[0], 5)
+	}
 }
