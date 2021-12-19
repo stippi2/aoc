e21fd106e94fba4d6e8675bbe5bc040bc5e80861
Stephan Aßmus <stephan.assmus@sap.com>
2021-12-19 19:48:31 +0100

    Prepare day 18

---
+++ 2021/day-18-snailfish-math/main_test.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+var exampleInput = []string{
+	"",
+}
+
+func Test_parseInput(t *testing.T) {
+	assert.True(t, true)
+}
