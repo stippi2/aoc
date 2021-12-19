613a0edc04d189bb85777e9d3494e73f75a697c7
Stephan Aßmus <stephan.assmus@sap.com>
2021-12-20 00:53:51 +0100

    Exploding seems to work

--- 2021/day-18-snailfish-math/main.go
+++ 2021/day-18-snailfish-math/main.go
@@ -1,14 +1,163 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
+	"strconv"
 	"strings"
 )
+
+type Node interface {
+	AddLeft(number int, previous Node) bool
+	AddRight(number int, previous Node) bool
+	Magnitude() int
+	Explode(level int) (Node, bool)
+}
+
+type RegularNumber struct {
+	value int
+}
+
+func (n *RegularNumber) AddLeft(number int, _ Node) bool {
+	n.value += number
+	return true
+}
+
+func (n *RegularNumber) AddRight(number int, _ Node) bool {
+	n.value += number
+	return true
+}
+
+func (n *RegularNumber) Magnitude() int {
+	return n.value
+}
+
+func (n *RegularNumber) Explode(_ int) (Node, bool) {
+	return n, false
+}
+
+func (n *RegularNumber) String() string {
+	return fmt.Sprintf("%v", n.value)
+}
+
+type Pair struct {
+	parent Node
+	left   Node
+	right  Node
+}
+
+func (p *Pair) AddLeft(number int, previous Node) bool {
+	if previous == p.right {
+		return p.left.AddLeft(number, p)
+	}
+	if previous == p.parent {
+		return p.left.AddLeft(number, p)
+	}
+	if p.parent == nil {
+		return false
+	}
+	return p.parent.AddLeft(number, p)
+}
+
+func (p *Pair) AddRight(number int, previous Node) bool {
+	if previous == p.left {
+		return p.right.AddRight(number, p)
+	}
+	if previous == p.parent {
+		return p.left.AddRight(number, p)
+	}
+	if p.parent == nil {
+		return false
+	}
+	return p.parent.AddRight(number, p)
+}
+
+func (p *Pair) Magnitude() int {
+	return p.left.Magnitude() * 3 + p.right.Magnitude() * 2
+}
+
+func (p *Pair) Explode(level int) (Node, bool) {
+	if level == 4 {
+		return &RegularNumber{0}, true
+	}
+	newLeft, explodedLeft := p.left.Explode(level + 1)
+	if explodedLeft {
+		if p.left != newLeft {
+			pair := p.left.(*Pair)
+			left := pair.left.Magnitude()
+			p.parent.AddLeft(left, p)
+			right := pair.right.Magnitude()
+			p.right.AddRight(right, p)
+			p.left = newLeft
+		}
+		return p, true
+	}
+	newRight, explodedRight := p.right.Explode(level + 1)
+	if explodedRight {
+		if p.right != newRight {
+			pair := p.right.(*Pair)
+			left := pair.left.Magnitude()
+			p.left.AddLeft(left, p)
+			right := pair.right.Magnitude()
+			p.parent.AddRight(right, p)
+			p.right = newRight
+		}
+		return p, true
+	}
+	return p, false
+}
+
+func (p *Pair) String() string {
+	return fmt.Sprintf("[%v,%v]", p.left, p.right)
+}
 
 func main() {
 }
 
-func parseInput(input string) {
+func add(left, right Node) Node {
+	result := &Pair{}
+	if leftPair, ok := left.(*Pair); ok {
+		leftPair.parent = result
+	}
+	if rightPair, ok := right.(*Pair); ok {
+		rightPair.parent = result
+	}
+	result.left = left
+	result.right = right
+	return result
+}
+
+func parseSnailfishNumber(line string) Node {
+	// trim the outer brackets (if any)
+	line = strings.TrimPrefix(line, "[")
+	line = strings.TrimSuffix(line, "]")
+
+	level := 0
+	for i := 0; i < len(line); i++ {
+		char := line[i:i+1]
+		if char == "[" {
+			level++
+		} else if char == "]" {
+			level--
+		} else if char == "," {
+			if level == 0 {
+				// This is a pair, and we found the "," on the pair's level
+				return add(parseSnailfishNumber(line[:i]), parseSnailfishNumber(line[i+1:]))
+			}
+		}
+	}
+	number, err := strconv.Atoi(line)
+	if err != nil {
+		panic(fmt.Sprintf("failed to parse '%s': %s", line, err))
+	}
+	return &RegularNumber{number}
+}
+
+func parseInput(input string) (numbers []interface{}) {
+	for _, line := range strings.Split(input, "\n") {
+		numbers = append(numbers, parseSnailfishNumber(line))
+	}
+	return
 }
 
 func loadInput(filename string) string {
--- 2021/day-18-snailfish-math/main_test.go
+++ 2021/day-18-snailfish-math/main_test.go
@@ -1,14 +1,60 @@
 package main
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-var exampleInput = []string{
-	"",
+func Test_parseInput(t *testing.T) {
+	type test struct {
+		example      string
+		expectedPair Node
+	}
+
+	var examples = []test{
+		{"[1,2]", &Pair{ nil,&RegularNumber{1}, &RegularNumber{2}}},
+		{"[[1,2],3]", add(&Pair{ nil, &RegularNumber{1}, &RegularNumber{2}}, &RegularNumber{3})},
+		{"[9,[8,7]]", add(&RegularNumber{9}, &Pair{ nil, &RegularNumber{8}, &RegularNumber{7}}) },
+		{"[[1,9],[8,5]]", add(&Pair{nil, &RegularNumber{1}, &RegularNumber{9}}, &Pair{nil, &RegularNumber{8}, &RegularNumber{5}}) },
+		{
+			example: "[[[[1,2],[3,4]],[[5,6],[7,8]]],9]",
+			expectedPair: add(
+				add(
+					add(
+						&Pair{nil, &RegularNumber{1}, &RegularNumber{2}},
+						&Pair{nil, &RegularNumber{3}, &RegularNumber{4}}),
+					add(
+						&Pair{nil, &RegularNumber{5}, &RegularNumber{6}},
+						&Pair{nil, &RegularNumber{7}, &RegularNumber{8}}),
+				),
+				&RegularNumber{9},
+			),
+		},
+	}
+
+	for _, e := range examples {
+		assert.Equal(t, e.expectedPair, parseSnailfishNumber(e.example))
+	}
 }
 
-func Test_parseInput(t *testing.T) {
-	assert.True(t, true)
+func Test_explode(t *testing.T) {
+	type test struct {
+		example  string
+		expected string
+	}
+
+	var explodeExamples = []test{
+		{"[[[[[9,8],1],2],3],4]", "[[[[0,9],2],3],4]"},
+		{"[7,[6,[5,[4,[3,2]]]]]", "[7,[6,[5,[7,0]]]]"},
+		{"[[6,[5,[4,[3,2]]]],1]", "[[6,[5,[7,0]]],3]"},
+		{"[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]", "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]"},
+		{"[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]", "[[3,[2,[8,0]]],[9,[5,[7,0]]]]"},
+	}
+
+	for _, e := range explodeExamples {
+		p := parseSnailfishNumber(e.example).(*Pair)
+		p.Explode(0)
+		assert.Equal(t, e.expected, fmt.Sprintf("%s", p))
+	}
 }
