6d80a9a97fb7fe93b1cf2cc6090f9e6fc1a6b88a
Stephan Aßmus <stephan.assmus@sap.com>
2021-12-20 18:05:50 +0100

    Prepare parsing test

--- 2021/day-19-beacon-scanner/main.go
+++ 2021/day-19-beacon-scanner/main.go
@@ -5,10 +5,29 @@ import (
 	"strings"
 )
 
+type Position struct {
+	x, y, z int
+}
+
+type Beacon struct {
+	// position is relative to the owning Scanner
+	position Position
+	// distancesTo12NearestBeacons is formed by computing the distance to every other beacon of the same scanner,
+	// sorting the resulting distances list, then using the 12 smallest distance values.
+	// The result should be independent of the owning Scanner's location and rotation,
+	// and can be used to find the same beacon in a different scanner.
+	distancesTo12NearestBeacons string
+}
+
+type Scanner struct {
+	beacons []Beacon
+}
+
 func main() {
 }
 
-func parseInput(input string) {
+func parseInput(input string) []Scanner {
+	return nil
 }
 
 func loadInput(filename string) string {
--- 2021/day-19-beacon-scanner/main_test.go
+++ 2021/day-19-beacon-scanner/main_test.go
@@ -5,6 +5,45 @@ import (
 	"testing"
 )
 
+var example = `--- scanner 0 ---
+-1,-1,1
+-2,-2,2
+-3,-3,3
+-2,-3,1
+5,6,-4
+8,0,7
+
+--- scanner 1 ---
+1,-1,1
+2,-2,2
+3,-3,3
+2,-1,3
+-5,4,-6
+-8,-7,0`
+
+var expected = []Scanner {
+	{
+		beacons: []Beacon{
+			{position: Position{-1,-1,1}},
+			{position: Position{-2,-2,2}},
+			{position: Position{-3,-3,3}},
+			{position: Position{-2,-3,1}},
+			{position: Position{5,6,-4}},
+			{position: Position{8,0,7}},
+		},
+	},
+	{
+		beacons: []Beacon{
+			{position: Position{1,-1,1}},
+			{position: Position{2,-2,2}},
+			{position: Position{3,-3,3}},
+			{position: Position{2,-1,3}},
+			{position: Position{-5,4,-6}},
+			{position: Position{-8,-7,0}},
+		},
+	},
+}
+
 func Test_parseInput(t *testing.T) {
-	assert.True(t, true)
+	assert.Equal(t, expected, parseInput(example))
 }
