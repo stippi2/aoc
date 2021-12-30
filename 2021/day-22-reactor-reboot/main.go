3c84c88014237d04ef8c0e9d3391bd3f30e7a77c
Stephan Aßmus <stephan.assmus@sap.com>
2021-12-30 11:49:48 +0100

    Parsing input works

--- 2021/day-22-reactor-reboot/main.go
+++ 2021/day-22-reactor-reboot/main.go
@@ -2,13 +2,101 @@ package main
 
 import (
 	"io/ioutil"
+	"strconv"
 	"strings"
 )
+
+type Position struct {
+	x, y, z int
+}
+
+func (p *Position) max(other Position) Position {
+	return Position{
+		x: max(p.x, other.x),
+		y: max(p.y, other.y),
+		z: max(p.z, other.z),
+	}
+}
+
+func (p *Position) min(other Position) Position {
+	return Position{
+		x: min(p.x, other.x),
+		y: min(p.y, other.y),
+		z: min(p.z, other.z),
+	}
+}
+
+type Volume struct {
+	min, max Position
+	on bool
+}
+
+func (v Volume) isValid() bool {
+	return v.min.x <= v.max.x && v.min.y <= v.max.y && v.min.z <= v.max.z
+}
+
+func (v Volume) intersect(other Volume) (Volume, bool) {
+	result := Volume{
+		min: v.min.max(other.min),
+		max: v.max.min(other.max),
+	}
+	return result, result.isValid()
+}
 
 func main() {
 }
 
-func parseInput(input string) {
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func abs(v int) int {
+	if v < 0 {
+		return -v
+	}
+	return v
+}
+
+func parseDimensions(dimensions []string, part int) Position {
+	dimensions[0] = strings.TrimPrefix(dimensions[0], "x=")
+	dimensions[1] = strings.TrimPrefix(dimensions[0], "y=")
+	dimensions[2] = strings.TrimPrefix(dimensions[0], "z=")
+	xMinMax := strings.Split(dimensions[0], "..")
+	yMinMax := strings.Split(dimensions[1], "..")
+	zMinMax := strings.Split(dimensions[2], "..")
+	x, _ := strconv.Atoi(xMinMax[part])
+	y, _ := strconv.Atoi(yMinMax[part])
+	z, _ := strconv.Atoi(zMinMax[part])
+	return Position{x, y, z}
+}
+
+func parseInput(input string) (volumes []Volume) {
+	lines := strings.Split(input, "\n")
+	for _, line := range lines {
+		parts := strings.Split(line, " ")
+		if len(parts) != 2 {
+			continue
+		}
+		dimensions := strings.Split(parts[1], ",")
+		if len(dimensions) != 3 {
+			continue
+		}
+		volume := Volume{on: parts[0] == "on"}
+		volume.min = parseDimensions(dimensions, 0)
+		volume.max = parseDimensions(dimensions, 1)
+		volumes = append(volumes, volume)
+	}
+	return
 }
 
 func loadInput(filename string) string {
--- 2021/day-22-reactor-reboot/main_test.go
+++ 2021/day-22-reactor-reboot/main_test.go
@@ -5,6 +5,18 @@ import (
 	"testing"
 )
 
+var smallExample = `on x=10..12,y=10..12,z=10..12
+on x=11..13,y=11..13,z=11..13
+off x=9..11,y=9..11,z=9..11
+on x=10..10,y=10..10,z=10..10`
+
 func Test_parseInput(t *testing.T) {
-	assert.True(t, true)
+	expected := []Volume{
+		{on: true, min: Position{10, 10, 10}, max: Position{12, 12, 12}},
+		{on: true, min: Position{11, 11, 11}, max: Position{13, 13, 13}},
+		{on: false, min: Position{9, 9, 9}, max: Position{11, 11, 11}},
+		{on: true, min: Position{10, 10, 10}, max: Position{10, 10, 10}},
+	}
+	actual := parseInput(smallExample)
+	assert.Equal(t, expected, actual)
 }
