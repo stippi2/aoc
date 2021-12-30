78c18a17143551c805c5c9611ac3dc8a9f07482c
Stephan Aßmus <stephan.assmus@sap.com>
2021-12-30 13:05:31 +0100

    WIP

--- 2021/day-22-reactor-reboot/main.go
+++ 2021/day-22-reactor-reboot/main.go
@@ -35,12 +35,207 @@ func (v Volume) isValid() bool {
 	return v.min.x <= v.max.x && v.min.y <= v.max.y && v.min.z <= v.max.z
 }
 
+func (v Volume) width() int {
+	return v.max.x - v.min.x + 1
+}
+
+func (v Volume) height() int {
+	return v.max.y - v.min.y + 1
+}
+
+func (v Volume) depth() int {
+	return v.max.z - v.min.z + 1
+}
 func (v Volume) intersect(other Volume) (Volume, bool) {
 	result := Volume{
 		min: v.min.max(other.min),
 		max: v.max.min(other.max),
 	}
 	return result, result.isValid()
+}
+
+func (v Volume) splitAlongX(x int) []Volume {
+	if x <= v.min.x || x >= v.max.x {
+		return []Volume{v}
+	}
+	return []Volume{
+		{
+			on: v.on,
+			min: Position{v.min.x, v.min.y, v.min.z},
+			max: Position{x, v.max.y, v.max.z},
+		},
+		{
+			on: v.on,
+			min: Position{x + 1, v.min.y, v.min.z},
+			max: Position{v.max.x, v.max.y, v.max.z},
+		},
+	}
+}
+
+func (v Volume) splitAlongY(y int) []Volume {
+	if y <= v.min.y || y >= v.max.y {
+		return []Volume{v}
+	}
+	return []Volume{
+		{
+			on: v.on,
+			min: Position{v.min.x, v.min.y, v.min.z},
+			max: Position{v.max.x, y, v.max.z},
+		},
+		{
+			on: v.on,
+			min: Position{v.min.x, y + 1, v.min.z},
+			max: Position{v.max.x, v.max.y, v.max.z},
+		},
+	}
+}
+
+func (v Volume) splitAlongZ(z int) []Volume {
+	if z <= v.min.z || z >= v.max.z {
+		return []Volume{v}
+	}
+	return []Volume{
+		{
+			on: v.on,
+			min: Position{v.min.x, v.min.y, v.min.z},
+			max: Position{v.max.x, v.max.y, z},
+		},
+		{
+			on: v.on,
+			min: Position{v.min.x, v.min.y, z + 1},
+			max: Position{v.max.x, v.max.y, v.max.z},
+		},
+	}
+}
+
+func (v Volume) contains(other Volume) bool {
+	return v.min.x <= other.min.x &&
+		v.min.y <= other.min.y &&
+		v.min.z <= other.min.z &&
+		v.max.x >= other.max.x &&
+		v.max.y >= other.max.y &&
+		v.max.z >= other.max.z
+}
+
+func splitAtX(volumes []Volume, x int) []Volume {
+	var result []Volume
+	for _, volume := range volumes {
+		result = append(result, volume.splitAlongX(x)...)
+	}
+	return result
+}
+
+func splitAtY(volumes []Volume, y int) []Volume {
+	var result []Volume
+	for _, volume := range volumes {
+		result = append(result, volume.splitAlongY(y)...)
+	}
+	return result
+}
+
+func splitAtZ(volumes []Volume, z int) []Volume {
+	var result []Volume
+	for _, volume := range volumes {
+		result = append(result, volume.splitAlongZ(z)...)
+	}
+	return result
+}
+
+func (v Volume) splitAt(intersection Volume) (volumes []Volume) {
+	volumes = []Volume{v}
+	volumes = splitAtX(volumes, intersection.min.x)
+	volumes = splitAtX(volumes, intersection.max.x)
+	volumes = splitAtY(volumes, intersection.min.y)
+	volumes = splitAtY(volumes, intersection.max.y)
+	volumes = splitAtZ(volumes, intersection.min.z)
+	volumes = splitAtZ(volumes, intersection.max.z)
+	return
+}
+
+func (v Volume) union(other Volume) (volumes []Volume) {
+	if v.contains(other) {
+		return []Volume{v}
+	}
+	if other.contains(v) {
+		return []Volume{other}
+	}
+	intersection, valid := v.intersect(other)
+	if !valid {
+		return []Volume{v, other}
+	}
+	uniqueVolumes := make(map[Volume]bool)
+	for _, volume := range v.splitAt(intersection) {
+		uniqueVolumes[volume] = true
+	}
+	for _, volume := range other.splitAt(intersection) {
+		uniqueVolumes[volume] = true
+	}
+	for volume := range uniqueVolumes {
+		volumes = append(volumes, volume)
+	}
+	return
+}
+
+func (v Volume) subtract(other Volume) (volumes []Volume) {
+	if !v.contains(other) {
+		return []Volume{v}
+	}
+	if other.contains(v) {
+		return []Volume{}
+	}
+	intersection, valid := v.intersect(other)
+	if !valid {
+		return []Volume{v, other}
+	}
+	uniqueVolumes := make(map[Volume]bool)
+	for _, volume := range v.splitAt(intersection) {
+		uniqueVolumes[volume] = true
+	}
+	for _, volume := range other.splitAt(intersection) {
+		delete(uniqueVolumes, volume)
+	}
+	for volume := range uniqueVolumes {
+		volumes = append(volumes, volume)
+	}
+	return
+}
+
+func countCubes(volumes []Volume, within Volume) int {
+	cubes := 0
+	for _, volume := range volumes {
+		intersection, valid := within.intersect(volume)
+		if !valid {
+			continue
+		}
+		cubes += intersection.width() * intersection.height() * intersection.depth()
+	}
+	return cubes
+}
+
+func rebootSequence(sequence []Volume) (volumes []Volume) {
+	for _, step := range sequence {
+		if len(volumes) == 0 && step.on {
+			volumes = append(volumes, step)
+			continue
+		}
+		uniqueVolumes := make(map[Volume]bool)
+		for _, volume := range volumes {
+			var parts []Volume
+			if step.on {
+				parts = volume.union(step)
+			} else {
+				parts = volume.subtract(step)
+			}
+			for _, part := range parts {
+				uniqueVolumes[part] = true
+			}
+		}
+		volumes = nil
+		for volume := range uniqueVolumes {
+			volumes = append(volumes, volume)
+		}
+	}
+	return
 }
 
 func main() {
