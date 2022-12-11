b17d6bc252f950f44894a25f735b647bb18da53f
Stephan Aßmus <stephan.assmus@sap.com>
2022-12-11 18:06:07 +0100

    Maybe part of the way towards part 2...

--- 2022/day-11-monkey-in-the-middle/main.go
+++ 2022/day-11-monkey-in-the-middle/main.go
@@ -2,15 +2,106 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"math"
 	"sort"
-	"strconv"
-	"strings"
 )
 
+type FactorizedNumber struct {
+	primeFactors map[int]int
+}
+
+func (n *FactorizedNumber) multiplyBy(prime int) {
+	n.primeFactors[prime] = n.primeFactors[prime] + 1
+}
+
+func (n *FactorizedNumber) multiplyBySelf() {
+	newPrimeFactors := map[int]int{}
+	for prime, potency := range n.primeFactors {
+		newPrimeFactors[prime] = potency * 2
+	}
+	n.primeFactors = newPrimeFactors
+}
+
+func (n *FactorizedNumber) add(value int) {
+	// 4, 5, 6, 7, 8
+
+	// 2*2 * 7*7 + 8  (196 + 8 = 204) = 2*2 * 3 * 17
+
+	// 2*7+8
+	// 2*11
+
+	// 2 * 7 + 8      (14 + 8 = 22)
+	// 2 * 11
+
+	// 3*3 * 5 + 4    (45 + 4 = 49)
+	// 7*7
+
+	// 2*2 * 11       (44 + 5 = 49)
+	// 7*7
+
+	defactorized := 1
+	for prime, potency := range n.primeFactors {
+		defactorized *= int(math.Pow(float64(prime), float64(potency)))
+	}
+	defactorized += value
+	newFactorized := toFactorizedNumber(defactorized)
+	n.primeFactors = newFactorized.primeFactors
+}
+
+func (n *FactorizedNumber) isDivisibleBy(prime int) bool {
+	// prime is one of 2, 3, 5, 7, 11, 13, 17, 19
+	return n.primeFactors[prime] > 0
+}
+
+func (n *FactorizedNumber) String() string {
+	result := ""
+	for prime, potency := range n.primeFactors {
+		if result != "" {
+			result += "*"
+		}
+		result += fmt.Sprintf("%v^%v", prime, potency)
+	}
+	return result
+}
+
+func toFactorizedNumber(n int) *FactorizedNumber {
+	// From: https://siongui.github.io/2017/05/09/go-find-all-prime-factors-of-integer-number/
+	fn := FactorizedNumber{primeFactors: map[int]int{}}
+	// Get the number of 2s that divide n
+	for n%2 == 0 {
+		fn.multiplyBy(2)
+		n = n / 2
+	}
+
+	// n must be odd at this point. so we can skip one element
+	// (note i = i + 2)
+	for i := 3; i*i <= n; i = i + 2 {
+		// while i divides n, append i and divide n
+		for n%i == 0 {
+			fn.multiplyBy(i)
+			n = n / i
+		}
+	}
+
+	// This condition is to handle the case when n is a prime number greater than 2
+	if n > 2 {
+		fn.multiplyBy(n)
+	}
+
+	return &fn
+}
+
+func convertToFactorizedNumbers(values []int) []*FactorizedNumber {
+	var factorizeNumbers []*FactorizedNumber
+	for _, value := range values {
+		factorizeNumbers = append(factorizeNumbers, toFactorizedNumber(value))
+	}
+	return factorizeNumbers
+}
+
 type Monkey struct {
-	items               []int
-	operation           func(old int) int
+	items               []*FactorizedNumber
+	operation           func(fn *FactorizedNumber)
 	testDivisor         int
 	targetMonkeyIfTrue  int
 	targetMonkeyIfFalse int
@@ -20,10 +111,9 @@ type Monkey struct {
 func monkeyTurn(monkeys []Monkey, current int) {
 	m := &monkeys[current]
 	for _, item := range m.items {
-		item = m.operation(item)
-		item /= 3
+		m.operation(item)
 		var targetMonkey int
-		if item%m.testDivisor == 0 {
+		if item.isDivisibleBy(m.testDivisor) {
 			targetMonkey = m.targetMonkeyIfTrue
 		} else {
 			targetMonkey = m.targetMonkeyIfFalse
@@ -31,7 +121,7 @@ func monkeyTurn(monkeys []Monkey, current int) {
 		monkeys[targetMonkey].items = append(monkeys[targetMonkey].items, item)
 	}
 	m.itemsProcessed += len(m.items)
-	m.items = []int{}
+	m.items = []*FactorizedNumber{}
 }
 
 func monkeyRound(monkeys []Monkey) {
@@ -44,78 +134,90 @@ func main() {
 	//monkeys := loadInput("puzzle-input.txt")
 	monkeys := []Monkey{
 		{
-			items: []int{78, 53, 89, 51, 52, 59, 58, 85},
-			operation: func(old int) int {
-				return old * 3
+			items: convertToFactorizedNumbers([]int{78, 53, 89, 51, 52, 59, 58, 85}),
+			operation: func(fn *FactorizedNumber) {
+				fn.multiplyBy(3)
 			},
 			testDivisor:         5,
 			targetMonkeyIfTrue:  2,
 			targetMonkeyIfFalse: 7,
 		},
 		{
-			items: []int{64},
-			operation: func(old int) int {
-				return old + 7
+			items: convertToFactorizedNumbers([]int{64}),
+			operation: func(fn *FactorizedNumber) {
+				fn.add(7)
 			},
 			testDivisor:         2,
 			targetMonkeyIfTrue:  3,
 			targetMonkeyIfFalse: 6,
 		},
 		{
-			items: []int{71, 93, 65, 82},
-			operation: func(old int) int {
-				return old + 5
+			items: convertToFactorizedNumbers([]int{71, 93, 65, 82}),
+			operation: func(fn *FactorizedNumber) {
+				fn.add(5)
 			},
 			testDivisor:         13,
 			targetMonkeyIfTrue:  5,
 			targetMonkeyIfFalse: 4,
 		},
 		{
-			items: []int{67, 73, 95, 75, 56, 74},
-			operation: func(old int) int {
-				return old + 8
+			items: convertToFactorizedNumbers([]int{67, 73, 95, 75, 56, 74}),
+			operation: func(fn *FactorizedNumber) {
+				fn.add(8)
 			},
 			testDivisor:         19,
 			targetMonkeyIfTrue:  6,
 			targetMonkeyIfFalse: 0,
 		},
 		{
-			items: []int{85, 91, 90},
-			operation: func(old int) int {
-				return old + 4
+			items: convertToFactorizedNumbers([]int{85, 91, 90}),
+			operation: func(fn *FactorizedNumber) {
+				fn.add(4)
 			},
 			testDivisor:         11,
 			targetMonkeyIfTrue:  3,
 			targetMonkeyIfFalse: 1,
 		},
 		{
-			items: []int{67, 96, 69, 55, 70, 83, 62},
-			operation: func(old int) int {
-				return old * 2
+			items: convertToFactorizedNumbers([]int{67, 96, 69, 55, 70, 83, 62}),
+			operation: func(fn *FactorizedNumber) {
+				fn.multiplyBy(2)
 			},
 			testDivisor:         3,
 			targetMonkeyIfTrue:  4,
 			targetMonkeyIfFalse: 1,
 		},
 		{
-			items: []int{53, 86, 98, 70, 64},
-			operation: func(old int) int {
-				return old + 6
+			items: convertToFactorizedNumbers([]int{53, 86, 98, 70, 64}),
+			operation: func(fn *FactorizedNumber) {
+				fn.add(6)
 			},
 			testDivisor:         7,
 			targetMonkeyIfTrue:  7,
 			targetMonkeyIfFalse: 0,
 		},
 		{
-			items: []int{88, 64},
-			operation: func(old int) int {
-				return old * old
+			items: convertToFactorizedNumbers([]int{88, 64}),
+			operation: func(fn *FactorizedNumber) {
+				fn.multiplyBySelf()
 			},
 			testDivisor:         17,
 			targetMonkeyIfTrue:  2,
 			targetMonkeyIfFalse: 5,
 		},
 	}
+
+	for i, monkey := range monkeys {
+		fmt.Printf("Monkey %v: [", i)
+		for j, item := range monkey.items {
+			if j > 0 {
+				fmt.Print(", ")
+			}
+			fmt.Print(item)
+		}
+		fmt.Print("]\n\n")
+	}
+
 	// part 1
 	for i := 0; i < 20; i++ {
 		monkeyRound(monkeys)
@@ -129,22 +231,23 @@ func main() {
 	fmt.Printf("monkey business: %v\n", monkeys[0].itemsProcessed*monkeys[1].itemsProcessed)
 }
 
-func parseInput(input string) []Monkey {
-	sections := strings.Split(input, "\n\n")
-	monkeys := make([]Monkey, len(sections))
-	for i, section := range sections {
-		lines := strings.Split(section, "\n")
-		items := strings.TrimPrefix(lines[1], "Starting items: ")
-		for _, item := range strings.Split(items, ", ") {
-			value, _ := strconv.Atoi(item)
-			monkeys[i].items = append(monkeys[i].items, value)
-		}
-		// ...
-	}
-	return monkeys
-}
-
-func loadInput(filename string) string {
-	fileContents, _ := os.ReadFile(filename)
-	return string(fileContents)
-}
+//
+//func parseInput(input string) []Monkey {
+//	sections := strings.Split(input, "\n\n")
+//	monkeys := make([]Monkey, len(sections))
+//	for i, section := range sections {
+//		lines := strings.Split(section, "\n")
+//		items := strings.TrimPrefix(lines[1], "Starting items: ")
+//		for _, item := range strings.Split(items, ", ") {
+//			value, _ := strconv.Atoi(item)
+//			monkeys[i].items = append(monkeys[i].items, value)
+//		}
+//		// ...
+//	}
+//	return monkeys
+//}
+//
+//func loadInput(filename string) string {
+//	fileContents, _ := os.ReadFile(filename)
+//	return string(fileContents)
+//}
