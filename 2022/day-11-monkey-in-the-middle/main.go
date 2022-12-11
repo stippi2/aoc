a4cca536b76f2de264348859432954352076b5bf
Stephan Aßmus <stephan.assmus@sap.com>
2022-12-11 20:14:00 +0100

    New idea: track remainders only

--- 2022/day-11-monkey-in-the-middle/main.go
+++ 2022/day-11-monkey-in-the-middle/main.go
@@ -2,110 +2,65 @@ package main
 
 import (
 	"fmt"
-	"math"
 	"sort"
 )
 
-type FactorizedNumber struct {
-	primeFactors map[int]int
+type RemainderNumber struct {
+	remainders map[int]int
 }
 
-func (n *FactorizedNumber) multiplyBy(prime int) {
-	n.primeFactors[prime] = n.primeFactors[prime] + 1
-}
-
-func (n *FactorizedNumber) multiplyBySelf() {
-	newPrimeFactors := map[int]int{}
-	for prime, potency := range n.primeFactors {
-		newPrimeFactors[prime] = potency * 2
+func (n *RemainderNumber) multiplyBy(value int) {
+	newRemainders := map[int]int{}
+	for prime, remainder := range n.remainders {
+		newRemainders[prime] = (remainder * value) % prime
 	}
-	n.primeFactors = newPrimeFactors
+	n.remainders = newRemainders
 }
 
-func (n *FactorizedNumber) add(value int) {
-	// 4, 5, 6, 7, 8
-
-	// 2*2 * 7*7 + 8  (196 + 8 = 204) = 2*2 * 3 * 17
-
-	// 2*7+8
-	// 2*11
-
-	// 2 * 7 + 8      (14 + 8 = 22)
-	// 2 * 11
-
-	// 3*3 * 5 + 4    (45 + 4 = 49)
-	// 7*7
-
-	// 2*2 * 11       (44 + 5 = 49)
-	// 7*7
-
-	defactorized := 1
-	for prime, potency := range n.primeFactors {
-		defactorized *= int(math.Pow(float64(prime), float64(potency)))
+func (n *RemainderNumber) multiplyBySelf() {
+	newRemainders := map[int]int{}
+	for prime, remainder := range n.remainders {
+		newRemainders[prime] = (remainder * remainder) % prime
 	}
-	defactorized += value
-	newFactorized := toFactorizedNumber(defactorized)
-	n.primeFactors = newFactorized.primeFactors
+	n.remainders = newRemainders
 }
 
-func (n *FactorizedNumber) isDivisibleBy(prime int) bool {
+func (n *RemainderNumber) add(value int) {
+	newRemainders := map[int]int{}
+	for prime, remainder := range n.remainders {
+		newRemainders[prime] = (remainder + value) % prime
+	}
+	n.remainders = newRemainders
+}
+
+func (n *RemainderNumber) isDivisibleBy(prime int) bool {
 	// prime is one of 2, 3, 5, 7, 11, 13, 17, 19
-	return n.primeFactors[prime] > 0
+	return n.remainders[prime] == 0
 }
 
-func (n *FactorizedNumber) String() string {
-	result := ""
-	for prime, potency := range n.primeFactors {
-		if result != "" {
-			result += "*"
-		}
-		result += fmt.Sprintf("%v^%v", prime, potency)
+func toRemainderNumber(n int) *RemainderNumber {
+	fn := RemainderNumber{remainders: map[int]int{}}
+	for _, prime := range []int{2, 3, 5, 7, 11, 13, 17, 19} {
+		fn.remainders[prime] = n % prime
 	}
-	return result
-}
-
-func toFactorizedNumber(n int) *FactorizedNumber {
-	// From: https://siongui.github.io/2017/05/09/go-find-all-prime-factors-of-integer-number/
-	fn := FactorizedNumber{primeFactors: map[int]int{}}
-	// Get the number of 2s that divide n
-	for n%2 == 0 {
-		fn.multiplyBy(2)
-		n = n / 2
-	}
-
-	// n must be odd at this point. so we can skip one element
-	// (note i = i + 2)
-	for i := 3; i*i <= n; i = i + 2 {
-		// while i divides n, append i and divide n
-		for n%i == 0 {
-			fn.multiplyBy(i)
-			n = n / i
-		}
-	}
-
-	// This condition is to handle the case when n is a prime number greater than 2
-	if n > 2 {
-		fn.multiplyBy(n)
-	}
-
 	return &fn
 }
 
-func convertToFactorizedNumbers(values []int) []*FactorizedNumber {
-	var factorizeNumbers []*FactorizedNumber
+func convertToRemainderNumbers(values []int) []*RemainderNumber {
+	var factorizeNumbers []*RemainderNumber
 	for _, value := range values {
-		factorizeNumbers = append(factorizeNumbers, toFactorizedNumber(value))
+		factorizeNumbers = append(factorizeNumbers, toRemainderNumber(value))
 	}
 	return factorizeNumbers
 }
 
 type Monkey struct {
-	items               []*FactorizedNumber
-	operation           func(fn *FactorizedNumber)
+	items               []*RemainderNumber
+	operation           func(fn *RemainderNumber)
 	testDivisor         int
 	targetMonkeyIfTrue  int
 	targetMonkeyIfFalse int
-	itemsProcessed      int
+	itemsProcessed      int64
 }
 
 func monkeyTurn(monkeys []Monkey, current int) {
@@ -120,8 +75,8 @@ func monkeyTurn(monkeys []Monkey, current int) {
 		}
 		monkeys[targetMonkey].items = append(monkeys[targetMonkey].items, item)
 	}
-	m.itemsProcessed += len(m.items)
-	m.items = []*FactorizedNumber{}
+	m.itemsProcessed += int64(len(m.items))
+	m.items = []*RemainderNumber{}
 }
 
 func monkeyRound(monkeys []Monkey) {
@@ -134,8 +89,8 @@ func main() {
 	//monkeys := loadInput("puzzle-input.txt")
 	monkeys := []Monkey{
 		{
-			items: convertToFactorizedNumbers([]int{78, 53, 89, 51, 52, 59, 58, 85}),
-			operation: func(fn *FactorizedNumber) {
+			items: convertToRemainderNumbers([]int{78, 53, 89, 51, 52, 59, 58, 85}),
+			operation: func(fn *RemainderNumber) {
 				fn.multiplyBy(3)
 			},
 			testDivisor:         5,
@@ -143,8 +98,8 @@ func main() {
 			targetMonkeyIfFalse: 7,
 		},
 		{
-			items: convertToFactorizedNumbers([]int{64}),
-			operation: func(fn *FactorizedNumber) {
+			items: convertToRemainderNumbers([]int{64}),
+			operation: func(fn *RemainderNumber) {
 				fn.add(7)
 			},
 			testDivisor:         2,
@@ -152,8 +107,8 @@ func main() {
 			targetMonkeyIfFalse: 6,
 		},
 		{
-			items: convertToFactorizedNumbers([]int{71, 93, 65, 82}),
-			operation: func(fn *FactorizedNumber) {
+			items: convertToRemainderNumbers([]int{71, 93, 65, 82}),
+			operation: func(fn *RemainderNumber) {
 				fn.add(5)
 			},
 			testDivisor:         13,
@@ -161,8 +116,8 @@ func main() {
 			targetMonkeyIfFalse: 4,
 		},
 		{
-			items: convertToFactorizedNumbers([]int{67, 73, 95, 75, 56, 74}),
-			operation: func(fn *FactorizedNumber) {
+			items: convertToRemainderNumbers([]int{67, 73, 95, 75, 56, 74}),
+			operation: func(fn *RemainderNumber) {
 				fn.add(8)
 			},
 			testDivisor:         19,
@@ -170,8 +125,8 @@ func main() {
 			targetMonkeyIfFalse: 0,
 		},
 		{
-			items: convertToFactorizedNumbers([]int{85, 91, 90}),
-			operation: func(fn *FactorizedNumber) {
+			items: convertToRemainderNumbers([]int{85, 91, 90}),
+			operation: func(fn *RemainderNumber) {
 				fn.add(4)
 			},
 			testDivisor:         11,
@@ -179,8 +134,8 @@ func main() {
 			targetMonkeyIfFalse: 1,
 		},
 		{
-			items: convertToFactorizedNumbers([]int{67, 96, 69, 55, 70, 83, 62}),
-			operation: func(fn *FactorizedNumber) {
+			items: convertToRemainderNumbers([]int{67, 96, 69, 55, 70, 83, 62}),
+			operation: func(fn *RemainderNumber) {
 				fn.multiplyBy(2)
 			},
 			testDivisor:         3,
@@ -188,8 +143,8 @@ func main() {
 			targetMonkeyIfFalse: 1,
 		},
 		{
-			items: convertToFactorizedNumbers([]int{53, 86, 98, 70, 64}),
-			operation: func(fn *FactorizedNumber) {
+			items: convertToRemainderNumbers([]int{53, 86, 98, 70, 64}),
+			operation: func(fn *RemainderNumber) {
 				fn.add(6)
 			},
 			testDivisor:         7,
@@ -197,8 +152,8 @@ func main() {
 			targetMonkeyIfFalse: 0,
 		},
 		{
-			items: convertToFactorizedNumbers([]int{88, 64}),
-			operation: func(fn *FactorizedNumber) {
+			items: convertToRemainderNumbers([]int{88, 64}),
+			operation: func(fn *RemainderNumber) {
 				fn.multiplyBySelf()
 			},
 			testDivisor:         17,
@@ -207,19 +162,8 @@ func main() {
 		},
 	}
 
-	for i, monkey := range monkeys {
-		fmt.Printf("Monkey %v: [", i)
-		for j, item := range monkey.items {
-			if j > 0 {
-				fmt.Print(", ")
-			}
-			fmt.Print(item)
-		}
-		fmt.Print("]\n\n")
-	}
-
 	// part 1
-	for i := 0; i < 20; i++ {
+	for i := 0; i < 10000; i++ {
 		monkeyRound(monkeys)
 	}
 	sort.Slice(monkeys, func(i, j int) bool {
--- 2022/day-11-monkey-in-the-middle/main_test.go
+++ 2022/day-11-monkey-in-the-middle/main_test.go
@@ -6,7 +6,17 @@ import (
 )
 
 func Test_add(t *testing.T) {
-	fn := toFactorizedNumber(45)
-	fn.add(4)
-	assert.True(t, fn.isDivisibleBy(7))
+	n := toRemainderNumber(45)
+	n.add(4)
+	assert.True(t, n.isDivisibleBy(7))
+}
+
+func Test_trackRemainderIdea(t *testing.T) {
+	v := 92
+	r := v % 7
+	assert.Equal(t, (v+5)%7, (r+5)%7)
+	assert.Equal(t, (v*3)%7, (r*3)%7)
+	assert.Equal(t, (v*2)%7, (r*2)%7)
+	assert.Equal(t, (v*v)%7, (r*v)%7)
+	assert.Equal(t, (v*v)%7, (r*r)%7)
 }
