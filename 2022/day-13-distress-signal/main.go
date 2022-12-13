d2b53e6fda6c779d9b2722ae9e2a3b77438af338
Stephan Aßmus <stephan.assmus@sap.com>
2022-12-13 17:34:06 +0100

    Day 13 part 1, not yet working

---
+++ 2022/day-13-distress-signal/main.go
@@ -0,0 +1,149 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const (
+	NoDecision int = 0
+	Sorted         = 1
+	Unsorted       = 2
+)
+
+func compare(left, right any) int {
+	lInt, lIsInt := left.(int)
+	rInt, rIsInt := right.(int)
+	// If both are integers:
+	if lIsInt && rIsInt {
+		if lInt < rInt {
+			return Sorted
+		} else if lInt > rInt {
+			return Unsorted
+		} else {
+			return NoDecision
+		}
+	}
+	// Mismatched types:
+	if lIsInt {
+		return compare([]any{lInt}, right)
+	}
+	if rIsInt {
+		return compare(left, []any{rInt})
+	}
+	// Compare lists:
+	leftList := left.([]any)
+	rightList := right.([]any)
+	leftLength := len(leftList)
+	rightLength := len(rightList)
+	for i := 0; i < leftLength; i++ {
+		if i >= rightLength {
+			return Unsorted
+		}
+		innerCompare := compare(leftList[i], rightList[i])
+		if innerCompare != NoDecision {
+			return innerCompare
+		}
+	}
+	if rightLength > leftLength {
+		return Sorted
+	}
+	return NoDecision
+}
+
+type Pair struct {
+	left, right any
+}
+
+func sumIndicesOrderedPairs(pairs []Pair) int {
+	sumIndices := 0
+	for i := 0; i < len(pairs); i++ {
+		if compare(pairs[i].left, pairs[i].right) == Sorted {
+			fmt.Printf("sorted: %v\n", i+1)
+			sumIndices += i + 1
+		}
+	}
+	return sumIndices
+}
+
+func main() {
+	pairs := parseInput(loadInput("puzzle-input.txt"))
+	fmt.Printf("sum of the indices of the ordered pairs: %v\n", sumIndicesOrderedPairs(pairs))
+}
+
+func parseItem(n string) any {
+	isArray := strings.HasPrefix(n, "[")
+	if isArray {
+		n = strings.TrimPrefix(n, "[")
+		n = strings.TrimSuffix(n, "]")
+	}
+
+	items := []any{}
+
+	start := 0
+	for start < len(n) {
+		var item any
+		if n[start] == '[' {
+			level := 1
+			end := len(n)
+			for j := start + 1; j < len(n); j++ {
+				switch n[j : j+1] {
+				case "[":
+					level++
+				case "]":
+					level--
+				}
+				if level == 0 {
+					end = j + 1
+					break
+				}
+			}
+			item = parseItem(n[start:end])
+			start = end
+		} else if n[start] >= '0' && n[start] <= '9' {
+			end := start + 1
+			for j := start + 1; j < len(n); j++ {
+				if n[j] < '0' || n[j] > '9' {
+					end = j
+					break
+				}
+			}
+			number, err := strconv.Atoi(n[start:end])
+			if err != nil {
+				panic(fmt.Sprintf("failed to parse '%s' as number: %s", n[start:end], err))
+			}
+			item = number
+			start = end
+		} else {
+			start++
+		}
+		if item != nil {
+			items = append(items, item)
+		}
+	}
+	if !isArray {
+		if len(items) > 1 {
+			panic("not an array?!")
+		}
+		return items[0]
+	}
+	return items
+}
+
+func parseInput(input string) []Pair {
+	pairSections := strings.Split(input, "\n\n")
+	pairs := make([]Pair, len(pairSections))
+	for i, section := range pairSections {
+		parts := strings.Split(section, "\n")
+		pairs[i].left = parseItem(parts[0])
+		pairs[i].right = parseItem(parts[1])
+	}
+	return pairs
+}
+
+func loadInput(filename string) string {
+	fileContents, _ := os.ReadFile(filename)
+	return string(fileContents)
+}
---
+++ 2022/day-13-distress-signal/main_test.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+var exampleInput = `[1,1,3,1,1]
+[1,1,5,1,1]
+
+[[1],[2,3,4]]
+[[1],4]
+
+[9]
+[[8,7,6]]
+
+[[4,4],4,4]
+[[4,4],4,4,4]
+
+[7,7,7,7]
+[7,7,7]
+
+[]
+[3]
+
+[[[]]]
+[[]]
+
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[1,[2,[3,[4,[5,6,0]]]],8,9]`
+
+func Test_part1(t *testing.T) {
+	pairs := parseInput(exampleInput)
+
+	assert.Equal(t, []Pair{
+		{
+			[]any{1, 1, 3, 1, 1},
+			[]any{1, 1, 5, 1, 1},
+		},
+		{
+			[]any{[]any{1}, []any{2, 3, 4}},
+			[]any{[]any{1}, 4},
+		},
+		{
+			[]any{9},
+			[]any{[]any{8, 7, 6}},
+		},
+		{
+			[]any{[]any{4, 4}, 4, 4},
+			[]any{[]any{4, 4}, 4, 4, 4},
+		},
+		{
+			[]any{7, 7, 7, 7},
+			[]any{7, 7, 7},
+		},
+		{
+			[]any{},
+			[]any{3},
+		},
+		{
+			[]any{[]any{[]any{}}},
+			[]any{[]any{}},
+		},
+		{
+			[]any{1, []any{2, []any{3, []any{4, []any{5, 6, 7}}}}, 8, 9},
+			[]any{1, []any{2, []any{3, []any{4, []any{5, 6, 0}}}}, 8, 9},
+		},
+	}, pairs)
+
+	assert.Equal(t, 13, sumIndicesOrderedPairs(pairs))
+}
+
+func Test_compare(t *testing.T) {
+	assert.Equal(t, Sorted, compare([]any{1, 1, 3, 1, 1}, []any{1, 1, 5, 1, 1}))
+
+	assert.Equal(t, Sorted, compare([]any{[]any{1}, []any{2, 3, 4}}, []any{[]any{1}, 4}))
+
+	assert.Equal(t, Unsorted, compare([]any{9}, []any{[]any{8, 7, 6}}))
+
+	assert.Equal(t, Sorted, compare([]any{[]any{4, 4}, 4, 4}, []any{[]any{4, 4}, 4, 4, 4}))
+
+	assert.Equal(t, Unsorted, compare([]any{7, 7, 7, 7}, []any{7, 7, 7}))
+
+	assert.Equal(t, Sorted, compare([]any{}, []any{3}))
+
+	assert.Equal(t, Unsorted, compare([]any{[]any{[]any{}}}, []any{[]any{}}))
+
+	assert.Equal(t, Unsorted, compare([]any{1, []any{2, []any{3, []any{4, []any{5, 6, 7}}}}, 8, 9}, []any{1, []any{2, []any{3, []any{4, []any{5, 6, 0}}}}, 8, 9}))
+}
