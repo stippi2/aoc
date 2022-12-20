1c9a2999ecd2bbd9bd3f0c772c4b889b035ce558
Stephan Aßmus <stephan.assmus@sap.com>
2022-12-20 16:58:31 +0100

    Day 19 WIP

---
+++ 2022/day-19-not-enough-minerals/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+type Blueprint struct {
+	costs map[string]map[string]int
+}
+
+type Inventory struct {
+	minerals map[string]int
+	robots map[string]int
+}
+
+func (i *Inventory) canBuildRobot(blueprint Blueprint, kind string) bool {
+	for mineral := range blueprint
+	return i.minerals[]
+}
+
+func main() {
+	//	blueprints := parseInput(loadInput("puzzle-input.txt"))
+}
+
+func parseInput(input string) []Blueprint {
+	lines := strings.Split(input, "\n")
+	blueprints := make([]Blueprint, len(lines))
+	minerals := []string{"ore", "clay", "obsidian", "geode"}
+	for i, line := range lines {
+		line = strings.TrimPrefix(line, fmt.Sprintf("Blueprint %d: ", i+1))
+		line = strings.TrimSuffix(line, ".")
+		parts := strings.Split(line, ". ")
+		blueprints[i].costs = make(map[string]map[string]int)
+		for j, robotKind := range minerals {
+			costsString := strings.TrimPrefix(parts[j], fmt.Sprintf("Each %s robot costs ", robotKind))
+			var costs1 int
+			var costs2 int
+			costs := make(map[string]int)
+			switch robotKind {
+			case "ore", "clay":
+				_, _ = fmt.Sscanf(costsString, "%d ore", &costs1)
+				costs["ore"] = costs1
+			case "obsidian":
+				_, _ = fmt.Sscanf(costsString, "%d ore and %d clay", &costs1, &costs2)
+				costs["ore"] = costs1
+				costs["clay"] = costs2
+			case "geode":
+				_, _ = fmt.Sscanf(costsString, "%d ore and %d obsidian", &costs1, &costs2)
+				costs["ore"] = costs1
+				costs["obsidian"] = costs2
+			}
+			blueprints[i].costs[robotKind] = costs
+		}
+	}
+	return blueprints
+}
+
+func loadInput(filename string) string {
+	fileContents, _ := os.ReadFile(filename)
+	return string(fileContents)
+}
---
+++ 2022/day-19-not-enough-minerals/main_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+var exampleInput = `Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.
+Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.`
+
+func Test_parseInput(t *testing.T) {
+	blueprints := parseInput(exampleInput)
+	if assert.Len(t, blueprints, 2) {
+		assert.Equal(t, Blueprint{costs: map[string]map[string]int{
+			"ore":      {"ore": 4},
+			"clay":     {"ore": 2},
+			"obsidian": {"ore": 3, "clay": 14},
+			"geode":    {"ore": 2, "obsidian": 7},
+		}}, blueprints[0])
+		assert.Equal(t, Blueprint{costs: map[string]map[string]int{
+			"ore":      {"ore": 2},
+			"clay":     {"ore": 3},
+			"obsidian": {"ore": 3, "clay": 8},
+			"geode":    {"ore": 3, "obsidian": 12},
+		}}, blueprints[1])
+	}
+}
