c5b823447891d1083703f3eaf220739a1c1fb177
Stephan Aßmus <stephan.assmus@sap.com>
2023-12-01 16:26:17 +0100

    2023 day 1 part 2

--- 2023/day-01-trebuchet/main.go
+++ 2023/day-01-trebuchet/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"os"
-	"strconv"
 	"strings"
 )
 
@@ -15,12 +14,48 @@ func sum(items []int) int {
 	return total
 }
 
-func main() {
-	numbers := parseInput(loadInput("puzzle-input.txt"))
-	fmt.Printf("The sum of the calibration numbers: %v\n", sum(numbers))
+var tokenToValuePart1 = map[string]int{
+	"1": 1,
+	"2": 2,
+	"3": 3,
+	"4": 4,
+	"5": 5,
+	"6": 6,
+	"7": 7,
+	"8": 8,
+	"9": 9,
 }
 
-func parseInput(input string) []int {
+var tokenToValuePart2 = map[string]int{
+	"one":   1,
+	"two":   2,
+	"three": 3,
+	"four":  4,
+	"five":  5,
+	"six":   6,
+	"seven": 7,
+	"eight": 8,
+	"nine":  9,
+	"1":     1,
+	"2":     2,
+	"3":     3,
+	"4":     4,
+	"5":     5,
+	"6":     6,
+	"7":     7,
+	"8":     8,
+	"9":     9,
+}
+
+func main() {
+	numbersPart1 := parseInput(loadInput("puzzle-input.txt"), tokenToValuePart1)
+	fmt.Printf("The sum of the calibration numbers (part 1): %v\n", sum(numbersPart1))
+
+	numbersPart2 := parseInput(loadInput("puzzle-input.txt"), tokenToValuePart2)
+	fmt.Printf("The sum of the calibration numbers (part 2): %v\n", sum(numbersPart2))
+}
+
+func parseInput(input string, tokens map[string]int) []int {
 	input = strings.TrimSpace(input)
 	lines := strings.Split(input, "\n")
 	fmt.Printf("Found %v lines\n", len(lines))
@@ -28,9 +63,14 @@ func parseInput(input string) []int {
 	for i, line := range lines {
 		var numbers []int = nil
 		for x := 0; x < len(line); x++ {
-			if line[x] >= '0' && line[x] <= '9' {
-				number, _ := strconv.Atoi(line[x : x+1])
-				numbers = append(numbers, number)
+			rest := line[x:]
+			for token, value := range tokens {
+				if strings.HasPrefix(rest, token) {
+					numbers = append(numbers, value)
+					// That would look sensible, but it's wrong:
+					// Consider "oneight" as in the example. We would miss the "eight" token.
+					// x += len(token) - 1
+				}
 			}
 		}
 		result[i] = 10*numbers[0] + numbers[len(numbers)-1]
--- 2023/day-01-trebuchet/main_test.go
+++ 2023/day-01-trebuchet/main_test.go
@@ -5,12 +5,24 @@ import (
 	"testing"
 )
 
-func Test_extractNumbers(t *testing.T) {
+func Test_extractNumbersPart1(t *testing.T) {
 	numbers := parseInput(`1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet
-`)
+`, tokenToValuePart1)
 	assert.Equal(t, []int{12, 38, 15, 77}, numbers)
 	assert.Equal(t, 142, sum(numbers))
+}
+
+func Test_extractNumbersPart2(t *testing.T) {
+	numbers := parseInput(`two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen`, tokenToValuePart2)
+	assert.Equal(t, []int{29, 83, 13, 24, 42, 14, 76}, numbers)
+	assert.Equal(t, 281, sum(numbers))
 }
