6a963566b6488003a17dfe68e22968383a460d66
Stephan Aßmus <stephan.assmus@sap.com>
2024-12-10 01:06:22 +0100

    Day 9 part 1 & 2

--- 2024/go/days/day09/day09.go
+++ 2024/go/days/day09/day09.go
@@ -4,15 +4,144 @@ import (
 	"aoc/2024/go/lib"
 )
 
-func calculateChecksum(_ string) int64 {
-	return 0
+func calculateDiskSize(input string) int64 {
+	size := int64(0)
+	for i := 0; i < len(input); i++ {
+		size += int64(input[i] - '0')
+	}
+	return size
+}
+
+func expandBlocks(input string, disk []int) {
+	currentFileID := 0
+	inFile := true
+	offset := 0
+	for i := 0; i < len(input); i++ {
+		repeat := int64(input[i] - '0')
+		for repeat > 0 {
+			if inFile {
+				disk[offset] = currentFileID
+			} else {
+				disk[offset] = -1
+			}
+			offset++
+			repeat--
+		}
+		if inFile {
+			currentFileID++
+		}
+		inFile = !inFile
+	}
+}
+
+func compactDisk(disk []int) {
+	offsetFront := 0
+	offsetBack := len(disk) - 1
+
+	// Move the back offset to the end of the last file in case the disk ends in free space
+	moveToEndOfNextFile := func() {
+		for disk[offsetBack] == -1 {
+			offsetBack--
+		}
+	}
+	moveToEndOfNextFile()
+
+	for offsetBack > offsetFront {
+		for offsetBack > offsetFront && disk[offsetFront] == -1 {
+			disk[offsetFront] = disk[offsetBack]
+			disk[offsetBack] = -1
+			offsetFront++
+			offsetBack--
+			moveToEndOfNextFile()
+		}
+		for offsetBack > offsetFront && disk[offsetFront] != -1 {
+			offsetFront++
+		}
+	}
+}
+
+func defragmentDisk(disk []int) {
+	offsetBack := len(disk) - 1
+	moveToEndOfNextFile := func() {
+		for disk[offsetBack] == -1 {
+			offsetBack--
+		}
+	}
+	moveToEndOfNextFile()
+
+	moveToNextFreeSpace := func(offset int) int {
+		for offset < len(disk) && disk[offset] != -1 {
+			offset++
+		}
+		return offset
+	}
+
+	slotLength := func(offset, direction int) int {
+		length := 0
+		fileID := disk[offset]
+		for offset >= 0 && offset < len(disk) && disk[offset] == fileID {
+			length += 1
+			offset += direction
+		}
+		return length
+	}
+
+	for offsetBack > 0 {
+		fileLength := slotLength(offsetBack, -1)
+		offsetFront := moveToNextFreeSpace(0)
+		fileMoved := false
+		for offsetFront < offsetBack-fileLength {
+			spaceLength := slotLength(offsetFront, 1)
+			if spaceLength >= fileLength {
+				fileID := disk[offsetBack]
+				for i := 0; i < fileLength; i++ {
+					disk[offsetFront] = fileID
+					disk[offsetBack] = -1
+					offsetFront++
+					offsetBack--
+				}
+				fileMoved = true
+				moveToEndOfNextFile()
+				break
+			} else {
+				offsetFront = moveToNextFreeSpace(offsetFront + spaceLength)
+			}
+		}
+		if !fileMoved {
+			offsetBack -= fileLength
+		}
+		if offsetBack < 0 {
+			break
+		}
+		moveToEndOfNextFile()
+	}
+}
+
+func calculateChecksum(disk []int) int64 {
+	checksum := int64(0)
+	offset := 0
+	for offset < len(disk) {
+		fileID := disk[offset]
+		if fileID != -1 {
+			checksum += int64(offset * fileID)
+		}
+		offset++
+	}
+	return checksum
 }
 
 func Part1() interface{} {
 	input, _ := lib.ReadInput(9)
-	return calculateChecksum(input)
+	disk := make([]int, calculateDiskSize(input))
+	expandBlocks(input, disk)
+	compactDisk(disk)
+	return calculateChecksum(disk)
 }
 
 func Part2() interface{} {
-	return "Not implemented"
+	input, _ := lib.ReadInput(9)
+	disk := make([]int, calculateDiskSize(input))
+	expandBlocks(input, disk)
+	defragmentDisk(disk)
+	return calculateChecksum(disk)
 }
--- 2024/go/days/day09/day09_test.go
+++ 2024/go/days/day09/day09_test.go
@@ -9,5 +9,37 @@ import (
 const example = "2333133121414131402"
 
 func Test_calculateChecksum(t *testing.T) {
-	assert.Equal(t, 1928, calculateChecksum(example))
+	disk := make([]int, calculateDiskSize(example))
+	expandBlocks(example, disk)
+	compactDisk(disk)
+	assert.Equal(t, int64(1928), calculateChecksum(disk))
+}
+
+func Test_calculateDiskSize(t *testing.T) {
+	assert.Equal(t, int64(6), calculateDiskSize("123"))
+}
+
+func Test_expandBlocks(t *testing.T) {
+	disk := make([]int, 6)
+	expandBlocks("123", disk)
+	assert.Equal(t, []int{0, -1, -1, 1, 1, 1}, disk)
+}
+
+func Test_expandBlocksExample(t *testing.T) {
+	disk := make([]int, calculateDiskSize(example))
+	expandBlocks(example, disk)
+	assert.Equal(t, []int{0, 0, -1, -1, -1, 1, 1, 1, -1, -1, -1, 2, -1, -1, -1, 3, 3, 3, -1, 4, 4, -1, 5, 5, 5, 5, -1, 6, 6, 6, 6, -1, 7, 7, 7, -1, 8, 8, 8, 8, 9, 9}, disk)
+}
+
+func Test_compactDisk(t *testing.T) {
+	disk := []int{0, -1, -1, 1, 1, 1, -1, 2, 2, -1, 3, -1}
+	compactDisk(disk)
+	assert.Equal(t, []int{0, 3, 2, 1, 1, 1, 2, -1, -1, -1, -1, -1}, disk)
+}
+
+func Test_defragmentDisk(t *testing.T) {
+	disk := make([]int, calculateDiskSize(example))
+	expandBlocks(example, disk)
+	defragmentDisk(disk)
+	assert.Equal(t, []int{0, 0, 9, 9, 2, 1, 1, 1, 7, 7, 7, -1, 4, 4, -1, 3, 3, 3, -1, -1, -1, -1, 5, 5, 5, 5, -1, 6, 6, 6, 6, -1, -1, -1, -1, -1, 8, 8, 8, 8, -1, -1}, disk)
 }
