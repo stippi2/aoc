31b8b72fb680df8612d18d325455bd68eee92af7
Stephan Aßmus <stephan.assmus@sap.com>
2024-12-14 22:06:57 +0100

    Day 10, part 2

--- 2024/go/days/day10/day10.go
+++ 2024/go/days/day10/day10.go
@@ -4,20 +4,18 @@ import (
 	"aoc/2024/go/lib"
 )
 
-func findTrails(grid *lib.Grid, start lib.Vec2) int {
-	reachableNines := make(map[lib.Vec2]bool)
-	visited := make(map[lib.Vec2]bool)
+func findTrails(grid *lib.Grid, start lib.Vec2) (int, int) {
+	reachableNines := make(map[lib.Vec2]int)
 
 	queue := []lib.Vec2{start}
 	for len(queue) > 0 {
 		pos := queue[len(queue)-1]
 		queue = queue[:len(queue)-1]
 
-		visited[pos] = true
 		height := grid.Get(pos.X, pos.Y) - '0'
 
 		if height == 9 {
-			reachableNines[pos] = true
+			reachableNines[pos] += 1
 			continue
 		}
 		neighbors := []lib.Vec2{
@@ -27,7 +25,7 @@ func findTrails(grid *lib.Grid, start lib.Vec2) int {
 			{X: pos.X, Y: pos.Y + 1},
 		}
 		for _, next := range neighbors {
-			if !grid.Contains(next.X, next.Y) || visited[next] {
+			if !grid.Contains(next.X, next.Y) {
 				continue
 			}
 			nextHeight := grid.Get(next.X, next.Y) - '0'
@@ -37,17 +35,26 @@ func findTrails(grid *lib.Grid, start lib.Vec2) int {
 		}
 	}
 
-	return len(reachableNines)
+	sumTrails := 0
+	for _, value := range reachableNines {
+		sumTrails += value
+	}
+	return len(reachableNines), sumTrails
 }
 
-func sumTrailHeads(input string) int {
+func sumTrailHeads(input string, countUniqueTrails bool) int {
 	grid := lib.NewGrid(input)
 	sum := 0
 	for y := 0; y < grid.Height(); y++ {
 		for x := 0; x < grid.Width(); x++ {
 			value := grid.Get(x, y) - '0'
 			if value == 0 {
-				sum += findTrails(grid, lib.Vec2{X: x, Y: y})
+				reachableNines, uniqueTrails := findTrails(grid, lib.Vec2{X: x, Y: y})
+				if countUniqueTrails {
+					sum += uniqueTrails
+				} else {
+					sum += reachableNines
+				}
 			}
 		}
 	}
@@ -56,9 +63,10 @@ func sumTrailHeads(input string) int {
 
 func Part1() int {
 	input, _ := lib.ReadInput(10)
-	return sumTrailHeads(input)
+	return sumTrailHeads(input, false)
 }
 
 func Part2() any {
-	return "Not implemented"
+	input, _ := lib.ReadInput(10)
+	return sumTrailHeads(input, true)
 }
--- 2024/go/days/day10/day10_test.go
+++ 2024/go/days/day10/day10_test.go
@@ -16,9 +16,9 @@ const example = `89010123
 10456732`
 
 func Test_part1(t *testing.T) {
-	assert.Equal(t, 36, sumTrailHeads(example))
+	assert.Equal(t, 36, sumTrailHeads(example, false))
 }
 
 func Test_part2(t *testing.T) {
-	assert.Equal(t, 81, sumTrailHeads(example))
+	assert.Equal(t, 81, sumTrailHeads(example, true))
 }
