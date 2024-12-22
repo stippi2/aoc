6b45ea92f421d39918750d01681ca8bd4871250c
Stephan Aßmus <stephan.assmus@sap.com>
2024-12-23 00:20:29 +0100

    Day 22 Part 2

--- 2024/go/days/day22/day22.go
+++ 2024/go/days/day22/day22.go
@@ -2,12 +2,15 @@ package day22
 
 import (
 	"aoc/2024/go/lib"
+	"fmt"
 	"strconv"
 	"strings"
 )
 
 type Buyer struct {
 	secretNumber int
+	prices       []int
+	changes      []int
 }
 
 func (b *Buyer) next() {
@@ -22,6 +25,68 @@ func (b *Buyer) next() {
 	t = b.secretNumber * 2048
 	b.secretNumber = b.secretNumber ^ t
 	b.secretNumber = b.secretNumber % 16777216
+}
+
+func (b *Buyer) generateSequence() {
+	b.prices = make([]int, 2001)
+	b.prices[0] = b.secretNumber % 10
+
+	for i := 1; i <= 2000; i++ {
+		b.next()
+		b.prices[i] = b.secretNumber % 10
+	}
+
+	b.changes = make([]int, 2000)
+	for i := 1; i <= 2000; i++ {
+		b.changes[i-1] = b.prices[i] - b.prices[i-1]
+	}
+}
+
+func findBestSequence(buyers []Buyer) (int, []int) {
+	bestSum := 0
+	bestSeq := make([]int, 4)
+
+	// For all possible sequences...
+	for d1 := -9; d1 <= 9; d1++ {
+		for d2 := -9; d2 <= 9; d2++ {
+			for d3 := -9; d3 <= 9; d3++ {
+				for d4 := -9; d4 <= 9; d4++ {
+					seq := []int{d1, d2, d3, d4}
+					sum := 0
+
+					// For every buyer...
+					for i := 0; i < len(buyers); i++ {
+						if pos := findSequence(buyers[i].changes, seq); pos >= 0 {
+							sum += buyers[i].prices[pos+4]
+						}
+					}
+
+					if sum > bestSum {
+						bestSum = sum
+						copy(bestSeq, seq)
+					}
+				}
+			}
+		}
+	}
+
+	return bestSum, bestSeq
+}
+
+func findSequence(changes []int, seq []int) int {
+	for i := 0; i <= len(changes)-4; i++ {
+		match := true
+		for j := 0; j < 4; j++ {
+			if changes[i+j] != seq[j] {
+				match = false
+				break
+			}
+		}
+		if match {
+			return i
+		}
+	}
+	return -1
 }
 
 func parseInput(input string) []Buyer {
@@ -51,5 +116,11 @@ func Part1() any {
 }
 
 func Part2() any {
-	return "Not implemented"
+	input, _ := lib.ReadInput(22)
+	buyers := parseInput(input)
+	for i := 0; i < len(buyers); i++ {
+		buyers[i].generateSequence()
+	}
+	mostBananas, bestSequence := findBestSequence(buyers)
+	return fmt.Sprintf("Most bananas: %v (sequence: %v)", mostBananas, bestSequence)
 }
--- 2024/go/days/day22/day22_test.go
+++ 2024/go/days/day22/day22_test.go
@@ -1,6 +1,7 @@
 package day22
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -14,4 +15,17 @@ const example = `1
 func Test_part1(t *testing.T) {
 	buyers := parseInput(example)
 	assert.Equal(t, 37327623, sum2000thSecretNumbers(buyers))
+}
+
+func Test_part2(t *testing.T) {
+	buyers := parseInput(`1
+2
+3
+2024`)
+	for i := 0; i < len(buyers); i++ {
+		buyers[i].generateSequence()
+	}
+	mostBananas, bestSequence := findBestSequence(buyers)
+	fmt.Printf("Most bananas: %v (sequence: %v)", mostBananas, bestSequence)
+	assert.Equal(t, 23, mostBananas)
 }
