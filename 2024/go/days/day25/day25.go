ab02223fe1fc412e6b0e327c88992dc1cd6bfa96
Stephan Aßmus <stephan.assmus@sap.com>
2024-12-25 23:17:49 +0100

    Day 25

--- 2024/go/days/day25/day25.go
+++ 2024/go/days/day25/day25.go
@@ -1,7 +1,76 @@
 package day25
 
+import (
+	"aoc/2024/go/lib"
+	"strings"
+)
+
+type Lock struct {
+	pins [5]int
+}
+
+type Key struct {
+	pins [5]int
+}
+
+func parseInput(input string) ([]*Lock, []*Key) {
+	var locks []*Lock
+	var keys []*Key
+
+	for _, grid := range strings.Split(input, "\n\n") {
+		lines := strings.Split(grid, "\n")
+		if lines[0] == "....." {
+			key := &Key{}
+			for y := 1; y < 6; y++ {
+				for x := 0; x < 5; x++ {
+					if lines[y][x] == '#' {
+						key.pins[x] = lib.Max(key.pins[x], 6-y)
+					}
+				}
+			}
+			keys = append(keys, key)
+		} else {
+			lock := &Lock{}
+			for y := 1; y < 6; y++ {
+				for x := 0; x < 5; x++ {
+					if lines[y][x] == '#' {
+						lock.pins[x] = y
+					}
+				}
+			}
+			locks = append(locks, lock)
+		}
+	}
+
+	return locks, keys
+}
+
+func keyFitsLock(key *Key, lock *Lock) bool {
+	for x := 0; x < 5; x++ {
+		if lock.pins[x]+key.pins[x] > 5 {
+			return false
+		}
+	}
+	return true
+}
+
+func countFittingLockKeyCombinations(locks []*Lock, keys []*Key) int {
+	sum := 0
+	for _, key := range keys {
+		for _, lock := range locks {
+			if keyFitsLock(key, lock) {
+				sum++
+			}
+		}
+	}
+
+	return sum
+}
+
 func Part1() any {
-	return "Not implemented"
+	input, _ := lib.ReadInput(25)
+	locks, keys := parseInput(input)
+	return countFittingLockKeyCombinations(locks, keys)
 }
 
 func Part2() any {
--- 2024/go/days/day25/day25_test.go
+++ 2024/go/days/day25/day25_test.go
@@ -6,8 +6,104 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-const example = ""
+const example = `#####
+.####
+.####
+.####
+.#.#.
+.#...
+.....
+
+#####
+##.##
+.#.##
+...##
+...#.
+...#.
+.....
+
+.....
+#....
+#....
+#...#
+#.#.#
+#.###
+#####
+
+.....
+.....
+#.#..
+###..
+###.#
+###.#
+#####
+
+.....
+.....
+.....
+#....
+#.#..
+#.#.#
+#####`
+
+func Test_parseInput(t *testing.T) {
+	test := `#####
+.####
+.####
+.####
+.#.#.
+.#...
+.....
+
+.....
+#....
+#....
+#...#
+#.#.#
+#.###
+#####`
+
+	lock := Lock{}
+	lock.pins[0] = 0
+	lock.pins[1] = 5
+	lock.pins[2] = 3
+	lock.pins[3] = 4
+	lock.pins[4] = 3
+
+	key := Key{}
+	key.pins[0] = 5
+	key.pins[1] = 0
+	key.pins[2] = 2
+	key.pins[3] = 1
+	key.pins[4] = 3
+
+	locks, keys := parseInput(test)
+	assert.Equal(t, lock, *locks[0])
+	assert.Equal(t, key, *keys[0])
+}
+
+func Test_keyFitsLock(t *testing.T) {
+	test := `#####
+.####
+.####
+.####
+.#.#.
+.#...
+.....
+
+.....
+.....
+.....
+#....
+#.#..
+#.#.#
+#####`
+
+	locks, keys := parseInput(test)
+	assert.True(t, keyFitsLock(keys[0], locks[0]))
+}
 
 func Test_part1(t *testing.T) {
-	assert.Equal(t, "", example)
+	locks, keys := parseInput(example)
+	assert.Equal(t, 3, countFittingLockKeyCombinations(locks, keys))
 }
