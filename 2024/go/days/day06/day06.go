434456b5152eb3f880c6746681f172da8c431fb6
Stephan Aßmus <stephan.assmus@sap.com>
2025-01-01 19:30:48 +0100

    Day 6 part 2

--- 2024/go/days/day06/day06.go
+++ 2024/go/days/day06/day06.go
@@ -13,34 +13,69 @@ func findStart(grid *lib.Grid) lib.Vec2 {
 	panic("start position not found")
 }
 
-func sumVisitedFields(input string) int {
-	grid := lib.NewGrid(input)
-	current := findStart(grid)
-	visited := make(map[lib.Vec2]bool)
+func trackGuardFields(grid *lib.Grid, start lib.Vec2, obstacle *lib.Vec2) (visited map[lib.Vec2]bool, trapped bool) {
+	current := start
+	visited = make(map[lib.Vec2]bool)
+
+	visitedByDirection := make(map[lib.Vec2]map[lib.Vec2]bool)
+	directions := []lib.Vec2{
+		{X: 0, Y: -1},
+		{X: 1, Y: 0},
+		{X: 0, Y: 1},
+		{X: -1, Y: 0},
+	}
+	for _, direction := range directions {
+		visitedByDirection[direction] = make(map[lib.Vec2]bool)
+	}
+
 	direction := lib.Vec2{X: 0, Y: -1}
 	for {
 		next := current.Add(direction)
 		visited[current] = true
+		if visitedByDirection[direction][current] {
+			return visited, true
+		}
+		visitedByDirection[direction][current] = true
 		if !grid.Contains(next.X, next.Y) {
 			break
 		}
-		switch grid.Get(next.X, next.Y) {
-		case '#':
+		if grid.Get(next.X, next.Y) == '#' || (obstacle != nil && *obstacle == next) {
 			// Next char is obstacle, rotate right 90°
 			direction = lib.Vec2{X: -direction.Y, Y: direction.X}
-		default:
+		} else {
 			// Continue in this direction
 			current = next
 		}
 	}
-	return len(visited)
+	return visited, false
 }
 
 func Part1() interface{} {
 	input, _ := lib.ReadInput(6)
-	return sumVisitedFields(input)
+	grid := lib.NewGrid(input)
+	start := findStart(grid)
+	fieldsVisited, _ := trackGuardFields(grid, start, nil)
+	return len(fieldsVisited)
+}
+
+func countLoopCausingObstacles(grid *lib.Grid, start lib.Vec2, visited map[lib.Vec2]bool) int {
+	loopCausingObstacles := 0
+	for position := range visited {
+		if position == start {
+			continue
+		}
+		_, isLoop := trackGuardFields(grid, start, &position)
+		if isLoop {
+			loopCausingObstacles++
+		}
+	}
+	return loopCausingObstacles
 }
 
 func Part2() interface{} {
-	return "Not implemented"
+	input, _ := lib.ReadInput(6)
+	grid := lib.NewGrid(input)
+	start := findStart(grid)
+	fieldsVisited, _ := trackGuardFields(grid, start, nil)
+	return countLoopCausingObstacles(grid, start, fieldsVisited)
 }
--- 2024/go/days/day06/day06_test.go
+++ 2024/go/days/day06/day06_test.go
@@ -1,6 +1,7 @@
 package day06
 
 import (
+	"aoc/2024/go/lib"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -18,8 +19,15 @@ const example = `....#.....
 ......#...`
 
 func Test_Part1(t *testing.T) {
-	assert.Equal(t, 41, sumVisitedFields(example))
+	grid := lib.NewGrid(example)
+	start := findStart(grid)
+	fieldsVisited, _ := trackGuardFields(grid, start, nil)
+	assert.Equal(t, 41, len(fieldsVisited))
 }
 
 func Test_Part2(t *testing.T) {
+	grid := lib.NewGrid(example)
+	start := findStart(grid)
+	fieldsVisited, _ := trackGuardFields(grid, start, nil)
+	assert.Equal(t, 6, countLoopCausingObstacles(grid, start, fieldsVisited))
 }
